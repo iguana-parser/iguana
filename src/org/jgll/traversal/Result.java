f4543355dd087607b03cffc6a8c47f47ba6a54a9
Ali Afroozeh <ali.afroozeh@cwi.nl>
2013-02-14 14:28:16 +0100

    Node interface added

---
+++ src/org/jgll/traversal/Result.java
@@ -0,0 +1,33 @@
+package org.jgll.traversal;
+
+public class Result<T> {
+	
+	private static final Result<Object> filter = new Result<Object>(null);
+	
+	private static final Result<Object> skip = new Result<Object>(null);
+
+	@SuppressWarnings("unchecked")
+	public static <K> Result<K> filter() {
+		return (Result<K>) filter;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <K> Result<K> skip() {
+		return (Result<K>) skip;
+	}	
+	
+	public static <K> Result<K> accept(K k) {
+		return new Result<K>(k);
+	}
+	
+	private T object;
+	
+	private Result(T object) {
+		this.object = object;
+	}
+	
+	public T getObject() {
+		return object;
+	}	
+}
+
