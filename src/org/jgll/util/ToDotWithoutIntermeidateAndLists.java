dbd634a5a22114ca45ea1ef644b1df8ff9477a60
Ali Afroozeh <afroozeh@gmail.com>
2013-05-30 00:45:03 +0200

    Removing layout from SPPF nodes for a better visualization.

--- src/org/jgll/util/ToDotWithoutIntermeidateAndLists.java
+++ src/org/jgll/util/ToDotWithoutIntermeidateAndLists.java
@@ -5,26 +5,25 @@ import static org.jgll.util.GraphVizUtil.SYMBOL_NODE;
 import org.jgll.sppf.ListSymbolNode;
 import org.jgll.sppf.NonterminalSymbolNode;
 import org.jgll.sppf.SPPFNode;
-import org.jgll.sppf.TerminalSymbolNode;
 import org.jgll.traversal.SPPFVisitorUtil;
 
 public class ToDotWithoutIntermeidateAndLists extends ToDotWithoutIntermediateNodes {
 	
 	@Override
 	public void visit(NonterminalSymbolNode node) {
-		super.visit(node);
+		SPPFVisitorUtil.removeIntermediateNode(node);
+		
 		if(!node.isVisited()) {
 			node.setVisited(true);
 	
 			sb.append("\"" + getId(node) + "\"" + String.format(SYMBOL_NODE, replaceWhiteSpace(node.toString())) + "\n");
-			
-			for (SPPFNode child : node.getChildren()) {
-				if(!(child instanceof TerminalSymbolNode && ((TerminalSymbolNode) child).getMatchedChar() == -2)) {
-					addEdgeToChild(node, child);					
+		
+			for(SPPFNode child : node.getChildren()) {
+				if(!child.getLabel().startsWith("layout(")) {
+				  addEdgeToChild(node, child);
+				  child.accept(this);
 				}
 			}
-			
-			SPPFVisitorUtil.visitChildren(node, this);
 		}
 	}
 	
