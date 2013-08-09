7b4743ea3772195a8e079d29a47371c08ec5c00d
Davy Landman <davy.landman@gmail.com>
2013-08-09 16:14:54 +0200

    Now also running the new hash functions

--- src/org/jgll/util/hashing/RunBenchmark.java
+++ src/org/jgll/util/hashing/RunBenchmark.java
@@ -9,12 +9,13 @@ public class RunBenchmark {
 				new MurmurHash2(r.nextInt())
 				, new MurmurHash3(r.nextInt())
 				, new SuperFastHash(r.nextInt())
+				, new SuperFastHash16BitOnly(r.nextInt())
+				, new Jenkins(r.nextInt())
+				, new JenkinsCWI(r.nextInt())
 		);
 		
 		bencher.bench(1*1000*1000, 100);
-		bencher.bench(2*1000*1000, 100);
 		bencher.bench(3*1000*1000, 100);
-		bencher.bench(4*1000*1000, 100);
 	}
 
 }
