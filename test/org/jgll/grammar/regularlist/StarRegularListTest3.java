89da5233c1c3041424d1961df288e4499f8834f6
Ali Afroozeh <afroozeh@gmail.com>
2013-10-01 16:23:46 +0200

    Test cases for the star regular lists added.

---
+++ test/org/jgll/grammar/regularlist/StarRegularListTest3.java
@@ -0,0 +1,85 @@
+package org.jgll.grammar.regularlist;
+
+import static org.jgll.util.CollectionsUtil.*;
+
+import org.jgll.grammar.Grammar;
+import org.jgll.grammar.GrammarBuilder;
+import org.jgll.grammar.symbols.Character;
+import org.jgll.grammar.symbols.CharacterClass;
+import org.jgll.grammar.symbols.Nonterminal;
+import org.jgll.grammar.symbols.Range;
+import org.jgll.grammar.symbols.RegularList;
+import org.jgll.grammar.symbols.Rule;
+import org.jgll.parser.GLLParser;
+import org.jgll.parser.ParseError;
+import org.jgll.parser.ParserFactory;
+import org.jgll.recognizer.GLLRecognizer;
+import org.jgll.sppf.NonterminalSymbolNode;
+import org.jgll.util.Input;
+import org.jgll.util.Visualization;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * S ::= Float;
+ * Float ::= [0-9]* [.] [0-9]* !>> [0-9] 
+ * 
+ * @author Ali Afroozeh
+ *
+ */
+public class StarRegularListTest3 {
+	
+	private Grammar grammar;
+	
+	private GLLParser levelParser;
+	private GLLParser rdParser;
+	private GLLRecognizer recognizer;
+
+	@Before
+	public void init() {
+		Nonterminal S = new Nonterminal("S");
+		Nonterminal Float = new Nonterminal("Float");
+		CharacterClass zero_nine = new CharacterClass(list(new Range('0', '9')));		
+		RegularList regularList = RegularList.star("[0-9]*", zero_nine);
+		
+		Rule r1 = new Rule(S, list(Float));
+		Rule r2 = new Rule(Float, list(new Nonterminal("[0-9]*"), new Character('.'), new Nonterminal("[0-9]*")));
+		
+		Rule r3 = new Rule(new Nonterminal("[0-9]*"), list(regularList));
+		
+		grammar = new GrammarBuilder().addRule(r1).addRule(r2).addRule(r3).build();
+		levelParser = ParserFactory.levelParser(grammar, 10);
+	}
+
+	@Test
+	public void test1() throws ParseError {
+		levelParser.parse(Input.fromString("123.123"), grammar, "S");
+	}
+
+	@Test
+	public void test2() throws ParseError {
+		levelParser.parse(Input.fromString("123456789012345.12345678901234567"), grammar, "S");
+	}
+	
+	@Test
+	public void test3() throws ParseError {
+		levelParser.parse(Input.fromString("1234567890123456789012345623434343.1234567890123456789012345678799898989889898"), grammar, "S");
+	}
+	
+	@Test
+	public void test4() throws ParseError {
+		levelParser.parse(Input.fromString(".123"), grammar, "S");
+	}
+	
+	@Test
+	public void test5() throws ParseError {
+		levelParser.parse(Input.fromString("123456789012345."), grammar, "S");
+	}
+	
+	@Test
+	public void test6() throws ParseError {
+		levelParser.parse(Input.fromString("."), grammar, "S");
+	}	
+
+
+}
