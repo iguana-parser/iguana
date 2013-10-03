1a63f7d8773377efaa11f6733fa0a1545185ee23
Ali Afroozeh <afroozeh@gmail.com>
2013-10-03 18:19:39 +0200

    Testing with DFAs

---
+++ test/org/jgll/automaton/AutomatonTest.java
@@ -0,0 +1,24 @@
+package org.jgll.automaton;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dk.brics.automaton.Automaton;
+import dk.brics.automaton.RegExp;
+import dk.brics.automaton.RunAutomaton;
+
+public class AutomatonTest {
+
+	@Test
+	public void test() {
+		
+		RegExp r = new RegExp("ab(c|d)*");
+		Automaton a = r.toAutomaton();
+		RunAutomaton ra = new RunAutomaton(a);
+		
+		for(int i = 0; i < 5; i++) {
+		}
+	}
+
+}
