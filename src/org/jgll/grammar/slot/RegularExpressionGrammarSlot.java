be6ed03fab42e09a7df6a9cb509bfe526d1f4e53
Ali Afroozeh <afroozeh@gmail.com>
2013-10-04 00:20:11 +0200

    updating regular expression slot

--- src/org/jgll/grammar/slot/RegularExpressionGrammarSlot.java
+++ src/org/jgll/grammar/slot/RegularExpressionGrammarSlot.java
@@ -1,5 +1,7 @@
 package org.jgll.grammar.slot;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Writer;
 
@@ -7,7 +9,12 @@ import org.jgll.grammar.symbols.RegularExpression;
 import org.jgll.grammar.symbols.Symbol;
 import org.jgll.parser.GLLParserInternals;
 import org.jgll.recognizer.GLLRecognizer;
+import org.jgll.sppf.DummyNode;
+import org.jgll.sppf.RegularListNode;
+import org.jgll.sppf.SPPFNode;
 import org.jgll.util.Input;
+
+import dk.brics.automaton.RunAutomaton;
 
 
 public class RegularExpressionGrammarSlot extends BodyGrammarSlot {
@@ -19,6 +26,77 @@ public class RegularExpressionGrammarSlot extends BodyGrammarSlot {
 		super(position, previous, head);
 		this.regexp = regexp;
 	}
+	
+	public RegularExpressionGrammarSlot copy(BodyGrammarSlot previous, HeadGrammarSlot head) {
+		RegularExpressionGrammarSlot slot = new RegularExpressionGrammarSlot(this.position, regexp, previous, head);
+		slot.preConditions = preConditions;
+		slot.popActions = popActions;
+		return slot;
+	}
+		
+	@Override
+	public GrammarSlot parse(GLLParserInternals parser, Input input) {
+		
+		int ci = parser.getCurrentInputIndex();
+		
+		int regularListLength = parser.getRegularListLength();
+		
+		RunAutomaton automaton = regexp.getAutomaton();
+
+		int state = 0;
+		
+		int l = 0;
+		for(int i = 0; i < regularListLength; i++) {
+			int charAtCi = input.charAt(ci + i);
+			
+			state = automaton.step(state, (char) charAtCi);
+			l++;
+			if(state == -1) {
+				break;
+			}
+		}
+		
+		// The regular node that is going to be created as the result of this
+		// slot action.
+		RegularListNode regularNode = parser.getRegularNode(this, ci, ci + l);
+
+		SPPFNode currentSPPFNode = parser.getCurrentSPPFNode();
+		
+		// If the current SPPF node is a partially matched list node, merge the nodes
+		if(currentSPPFNode instanceof RegularListNode && ((RegularListNode) currentSPPFNode).isPartial()) {
+			regularNode = parser.getRegularNode(this, currentSPPFNode.getLeftExtent(), ci + l);
+		}
+
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		try {
+			automaton.store(output);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+		// If the whole list is matched
+		if(l <= regularListLength && automaton.step(state, (char) input.charAt(ci + l)) != -1) {
+			parser.setCurrentSPPFNode(DummyNode.getInstance());
+			parser.getNonterminalNode((LastGrammarSlot) next, regularNode);
+			parser.pop();
+			return null;
+		} 
+		else {
+			try {
+				automaton =  RunAutomaton.load(new ByteArrayInputStream(output.toByteArray()));
+			}
+			catch (ClassCastException | ClassNotFoundException | IOException e) {
+				e.printStackTrace();
+			}
+			regularNode.setPartial(true);
+			parser.addDescriptor(this, parser.getCurrentGSSNode(), ci + l, regularNode);
+		}
+		
+		return null;
+	}
+
 
 	@Override
 	public boolean testFirstSet(int index, Input input) {
@@ -60,12 +138,6 @@ public class RegularExpressionGrammarSlot extends BodyGrammarSlot {
 	public void codeParser(Writer writer) throws IOException {
 		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public GrammarSlot parse(GLLParserInternals parser, Input input) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
