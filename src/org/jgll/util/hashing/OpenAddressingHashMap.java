68ef3ac266e4d0af4274091c58edb72a7931f3b9
Ali Afroozeh <afroozeh@gmail.com>
2013-11-25 00:16:31 +0100

    OpenAddressing maps are back again.

---
+++ src/org/jgll/util/hashing/OpenAddressingHashMap.java
@@ -0,0 +1,210 @@
+package org.jgll.util.hashing;
+
+import java.util.Collection;
+import org.jgll.util.KeyValue;
+import java.util.Map;
+import java.util.Set;
+
+public class OpenAddressingHashMap<K, V> implements Map<K, V> {
+	
+	/**
+	 * The constant suggested by Knuth for multiplicative hashing
+	 */
+	private static final long a = 2654435769L;
+	
+	private static final int DEFAULT_INITIAL_CAPACITY = 16;
+	private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+	
+	private Entry<K, V>[] table;
+	
+	private int capacity;
+	
+	private int threshold;
+	
+	private final float loadFactor;
+	
+	private int size;
+	
+	private int rehashCount;
+	
+	/**
+	 * capacity - 1
+	 * The bitMask is used to get the p most-significant bytes of
+	 * the multiplicaiton.
+	 */
+	private int bitMask;
+	
+	/**
+	 * capacity = 2 ^ p
+	 */
+	private int p;
+	
+	public OpenAddressingHashMap() {
+		this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+	}
+	
+	public OpenAddressingHashMap(int initialCapcity) {
+		this(initialCapcity, DEFAULT_LOAD_FACTOR);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public OpenAddressingHashMap(int initialCapacity, float loadFactor) {
+		this.loadFactor = loadFactor;
+		
+		capacity = 1;
+        while (capacity < initialCapacity) {
+            capacity <<= 1;
+            p++;
+        }
+		
+		bitMask = capacity - 1;
+		
+		threshold = (int) (loadFactor * capacity);
+		this.table = new Entry[capacity];
+	}
+	
+	@Override
+	public V get(Object key) {
+		
+		int j = indexFor(key.hashCode());
+		
+		do {
+			if(table[j] == null) {
+				return null;
+			}
+			
+			if(table[j].getKey().equals(key)) {
+				return table[j].getValue();
+			}
+			
+			j = next(j);
+			
+		} while(true);
+	}
+	
+	public V put(K key, V value) {		
+		int j = indexFor(key.hashCode());
+		
+		do {
+			if(table[j] == null) {
+				table[j] = new KeyValue<>(key, value);
+				size++;
+				if (size >= threshold) {
+					rehash();
+				}
+				return null;
+			}
+			
+			else if(table[j].getKey().equals(key)) {
+				V oldValue = table[j].getValue();
+				table[j].setValue(value);
+				return oldValue;
+			}
+			
+			j = next(j);
+		} while(true);
+	}
+	
+	public void delete(K key) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	private void rehash() {
+		capacity <<= 1;
+		p += 1;
+		bitMask = capacity - 1;
+		
+		@SuppressWarnings("unchecked")
+		Entry<K, V>[] newTable = new KeyValue[capacity];
+		
+		label:
+		for(Entry<K, V> entry : table) {
+			if(entry != null) {
+				
+				int j = indexFor(entry.getKey().hashCode());
+				
+				do {
+					if(newTable[j] == null) {
+						newTable[j] = entry;
+						continue label;
+					}
+					
+					j = next(j);
+				} while(true);
+			}
+		}
+		
+		table = newTable;
+		
+		threshold = (int) (loadFactor * capacity);
+		rehashCount++;
+	}
+	
+	private int indexFor(int hash) {
+		return  ((int)(a * hash) >> (32 - p)) & bitMask;		
+	}
+		
+	private int next(int j) {
+		j = j - 1;
+		if(j < 0) {
+			j += capacity;
+		}
+		return j;
+	}
+		
+	public int getRehashCount() {
+		return rehashCount;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public V remove(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		throw new UnsupportedOperationException();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void clear() {
+		table = new Entry[capacity];
+		size = 0;
+	}
+
+	@Override
+	public Set<K> keySet() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Collection<V> values() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/org/jgll/util/hashing/OpenAddressingHashSet.java
@@ -0,0 +1,198 @@
+package org.jgll.util.hashing;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class OpenAddressingHashSet<E> implements Set<E> {
+	
+	private static final int DEFAULT_INITIAL_CAPACITY = 32;
+	private static final float DEFAULT_LOAD_FACTOR = 0.5f;
+	
+	private int capacity;
+	
+	private int size;
+	
+//	private int threshold;
+	
+	private float loadFactor;
+	
+	private int rehashCount;
+
+	/**
+	 * capacity - 1
+	 * The bitMask is used to get the p most-significant bytes of
+	 * the multiplicaiton.
+	 */
+	private int bitMask;
+	
+	/**
+	 * capacity = 2 ^ p
+	 */
+	private int p;
+	
+	private Object[] table;
+	
+	public OpenAddressingHashSet() {
+		this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+	}
+	
+	public OpenAddressingHashSet(int initalCapacity) {
+		this(initalCapacity, DEFAULT_LOAD_FACTOR);
+	}
+	
+	public OpenAddressingHashSet(int initialCapacity, float loadFactor) {
+		this.loadFactor = loadFactor;
+
+		initialCapacity = Math.max(4, initialCapacity);
+		initialCapacity /= loadFactor;
+		
+		capacity = 1;
+        while (capacity < initialCapacity) {
+            capacity <<= 1;
+            p++;
+        }
+        
+		bitMask = capacity - 1;
+		
+//		threshold = (int) (loadFactor * capacity);
+		table = new Object[capacity];
+	}
+	
+	@Override
+	public boolean contains(Object key) {
+		
+		int i = indexFor(key.hashCode());
+		
+		while(table[i] != null && !table[i].equals(key)) {			
+			i = (i + 1) & bitMask; // mod capacity
+		}
+		
+		return table[i] != null;
+	}
+	
+	@Override
+	public boolean add(Object key) {
+				
+		int i = indexFor(key.hashCode());
+		do {
+			if(table[i] == null) {
+				table[i] = key;
+				size++;
+//				if (size >= threshold) {
+//					rehash();
+//				}
+				return true;
+			}
+			
+			else if(table[i].equals(key)) {
+				return false;
+			}
+			
+			i = (i + 1) & bitMask; // mod capacity
+		} while(true);
+		
+	}
+	
+	
+	private void rehash() {
+		throw new IllegalStateException();
+//		capacity <<= 1;
+//		p += 1;
+//		bitMask = capacity - 1;
+//		
+//		Object[] newTable = new Object[capacity];
+//		
+//		label:
+//		for(Object key : table) {
+//			if(key != null) {
+//				
+//				int j = indexFor(key.hashCode());
+//
+//				do {
+//					if(newTable[j] == null) {
+//						newTable[j] = key;
+//						continue label;
+//					}
+//					
+//					j = next(j);
+//				} while(true);
+//			}
+//		}
+//		table = newTable;
+//		
+//		threshold = (int) (loadFactor * capacity);
+//		rehashCount++;
+	}
+	
+	private int indexFor(int hash) {
+		return  hash & bitMask;		
+	}
+		
+	private int next(int j) {
+		if(j == 0) {
+			return capacity - 1;
+		}
+		return j - 1;
+	}
+
+	public int getRehashCount() {
+		return rehashCount;
+	}
+	
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return false;
+	}
+
+	@Override
+	public void clear() {
+	}
+
+}
