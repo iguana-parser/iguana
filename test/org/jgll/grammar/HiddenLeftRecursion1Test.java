f62049e9d1788258e64447f3d959149b81db2417
Ali Afroozeh <afroozeh@gmail.com>
2013-12-02 23:20:28 +0100

    Test case for hidden left recursion added.

--- test/org/jgll/grammar/HiddenLeftRecursion1Test.java
+++ test/org/jgll/grammar/HiddenLeftRecursion1Test.java
@@ -17,11 +17,13 @@ import org.junit.Test;
 
 /**
  * 
- * A ::= B A c
+ * A ::= B A a
+ *     | D A b
+ *     | c
  * 
- * B ::= b | epsilon
+ * B ::= x | epsilon
  * 
- * A ::= a
+ * D ::= y | epsilon
  * 
  * 
  * @author Ali Afroozeh
@@ -37,20 +39,30 @@ public class HiddenLeftRecursion1Test {
 	public void init() {
 		Nonterminal A = new Nonterminal("A");
 		Nonterminal B = new Nonterminal("B");
+		Nonterminal D = new Nonterminal("D");
 		
 		Character a = new Character('a');
 		Character b = new Character('b');
 		Character c = new Character('c');
+		Character x = new Character('x');
+		Character y = new Character('y');
 		
-		Rule r1 = new Rule(A, list(B, A, c));
-		Rule r4 = new Rule(A, list(a));
-		Rule r2 = new Rule(B, list(b));
-		Rule r3 = new Rule(B);
+		Rule r1 = new Rule(A, list(B, A, a));
+		Rule r2 = new Rule(A, list(D, A, b));
+		Rule r3 = new Rule(A, list(c));
+		Rule r4 = new Rule(B, list(x));
+		Rule r5 = new Rule(B);
+		Rule r6 = new Rule(D, list(y));
+		Rule r7 = new Rule(D);
+		
 
 		builder = new GrammarBuilder("IndirectRecursion").addRule(r1)
 														 .addRule(r2)
 														 .addRule(r3)
-														 .addRule(r4);
+														 .addRule(r4)
+														 .addRule(r5)
+														 .addRule(r6)
+														 .addRule(r7);
 			
 		grammar = builder.build();
 		rdParser = ParserFactory.createRecursiveDescentParser(grammar);
@@ -58,8 +70,11 @@ public class HiddenLeftRecursion1Test {
 	
 	@Test
 	public void test() throws ParseError {
-		NonterminalSymbolNode sppf = rdParser.parse(Input.fromString("bac"), grammar, "A");
-		Visualization.generateGSSGraph("/Users/ali/output", ((GLLParserImpl) rdParser).getLookupTable().getGSSNodes());
+		NonterminalSymbolNode sppf1 = rdParser.parse(Input.fromString("xca"), grammar, "A");
+		NonterminalSymbolNode sppf2 = rdParser.parse(Input.fromString("ycb"), grammar, "A");
+		NonterminalSymbolNode sppf3 = rdParser.parse(Input.fromString("cababaab"), grammar, "A");
+		NonterminalSymbolNode sppf4 = rdParser.parse(Input.fromString("xcabbbbb"), grammar, "A");
+		NonterminalSymbolNode sppf5 = rdParser.parse(Input.fromString("ycaaaabaaaa"), grammar, "A");
 	}
 	
 }
