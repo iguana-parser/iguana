9402d3caf92cb8a09b7429970ee446adacfff5e0
Ali Afroozeh <afroozeh@gmail.com>
2013-12-14 00:25:56 +0100

    More efficient tokenization.

--- src/org/jgll/lexer/GLLLexerImpl.java
+++ src/org/jgll/lexer/GLLLexerImpl.java
@@ -74,41 +74,56 @@ public class GLLLexerImpl implements GLLLexer {
 	}
 	
 	private void tokenize(String input) {
-		for(int i = 0; i < input.length(); i++) {
-			for(Symbol symbol : grammar.getTokens()) {
-				if(symbol instanceof Keyword) {
-					tokenize(i, input, (Keyword) symbol);
-				} 
-				else if (symbol instanceof RegularExpression) {
-					tokenize(i, input, (RegularExpression) symbol);
-				}
-				else if(symbol instanceof Terminal) {
-					tokenize(i, input, (Terminal) symbol);
-				}
-			}
-		}
-		
 		tokens[input.length() - 1][EOF] = 0;
 		tokenIDs[input.length() - 1].set(EOF);
+		tokenize(input, 0);
 	}
 	
-	private void tokenize(int inputIndex, String input, Keyword keyword) {
-		if(keyword.match(input, inputIndex)) {
-			createToken(inputIndex, keyword, keyword.size());
+	private void tokenize(String input, int index) {
+		for(Symbol symbol : grammar.getTokens()) {
+			int length;
+			if(symbol instanceof Keyword) {
+				length = tokenize(index, input, (Keyword) symbol);
+			} 
+			else if (symbol instanceof RegularExpression) {
+				length = tokenize(index, input, (RegularExpression) symbol);
+			}
+			else if(symbol instanceof Terminal) {
+				length = tokenize(index, input, (Terminal) symbol);
+			} else {
+				length = -1;
+			}
+			
+			if(length > 0 && index + length < input.length()) {
+				tokenize(input, index + length);					
+				return;
+			}
 		}
 	}
 	
-	private void tokenize(int inputIndex, String input, RegularExpression regex) {
+	private int tokenize(int inputIndex, String input, Keyword keyword) {
+		if(keyword.match(input, inputIndex)) {
+			createToken(inputIndex, keyword, keyword.size());
+			return keyword.size();
+		}
+		return -1;
+	}
+	
+	private int tokenize(int inputIndex, String input, RegularExpression regex) {
 		int length = regex.getAutomaton().run(input, inputIndex);
 		if(length != -1) {
 			createToken(inputIndex, regex, length);
+			return length;
 		}
+		return -1;
 	}
 	
-	private void tokenize(int inputIndex, String input, Terminal terminal) {
+	private int tokenize(int inputIndex, String input, Terminal terminal) {
 		if(terminal.match(input.charAt(inputIndex))) {
 			createToken(inputIndex, terminal, 1);
+			return 1;
 		}
+		return -1;
 	}
 	
 
