5d7bd53d133c09689083017b0fdf0fc9fa886001
Ali Afroozeh <afroozeh@gmail.com>
2014-01-01 22:17:31 +0100

    Another regexp example added.

--- test/org/jgll/regex/Examples.java
+++ test/org/jgll/regex/Examples.java
@@ -2,6 +2,7 @@ package org.jgll.regex;
 
 import static org.junit.Assert.*;
 
+import org.jgll.grammar.symbol.Character;
 import org.jgll.grammar.symbol.CharacterClass;
 import org.jgll.grammar.symbol.Range;
 import org.jgll.util.Input;
@@ -36,8 +37,20 @@ public class Examples {
 	 */
 	@Test
 	public void test2() {
-		CharacterClass c1 = new CharacterClass(new Range('0', '9'));
-		CharacterClass c2 = new CharacterClass(new Range('0', '9'));
+		CharacterClass c = new CharacterClass(new Range('0', '9'));
+		RegularExpression regexp = new Sequence(new RegexPlus(c), new Character('.'), new RegexPlus(c));
+		
+		NFA nfa = regexp.toNFA();
+		
+		DFA dfa = nfa.toDFA();
+
+		assertTrue(dfa.match(Input.fromString("1.2")));
+		assertFalse(dfa.match(Input.fromString("9")));
+		assertFalse(dfa.match(Input.fromString(".9")));
+		assertFalse(dfa.match(Input.fromString("123.")));
+		assertTrue(dfa.match(Input.fromString("12.2")));
+		assertTrue(dfa.match(Input.fromString("1342343.27890")));
+		assertTrue(dfa.match(Input.fromString("908397439483.278902433")));
 	}
 	
 
