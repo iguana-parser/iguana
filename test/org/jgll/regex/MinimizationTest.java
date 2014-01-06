39c91c63a03f5c686099836219adc105469add13
Ali Afroozeh <afroozeh@gmail.com>
2014-01-06 02:38:11 +0100

    Another minimization test added.

--- test/org/jgll/regex/MinimizationTest.java
+++ test/org/jgll/regex/MinimizationTest.java
@@ -2,8 +2,6 @@ package org.jgll.regex;
 
 import static org.junit.Assert.*;
 
-import org.jgll.util.dot.GraphVizUtil;
-import org.jgll.util.dot.NFAToDot;
 import org.junit.Test;
 
 public class MinimizationTest {
@@ -141,5 +139,55 @@ public class MinimizationTest {
 		state1.addTransition(new Transition(49, 49, state2));
 		return new NFA(state1);
 	}
-
+	
+	@Test
+	public void test4() {
+		State a = new State();
+		State b = new State();
+		State c = new State();
+		State d = new State();
+		State e = new State();
+		State f = new State();
+		State g = new State();
+		State h = new State();
+		State i = new State(true);
+		
+		a.addTransition(new Transition('1', b));
+		a.addTransition(new Transition('2', c));
+		a.addTransition(new Transition('3', d));
+		b.addTransition(new Transition('1', '3', e));
+		c.addTransition(new Transition('1', '3', e));
+		d.addTransition(new Transition('1', '3', e));
+		e.addTransition(new Transition('1', f));
+		e.addTransition(new Transition('2', g));
+		e.addTransition(new Transition('3', h));
+		f.addTransition(new Transition('1', '3', i));
+		g.addTransition(new Transition('1', '3', i));
+		h.addTransition(new Transition('1', i));
+		
+		NFA nfa = new NFA(a);
+		
+		NFA minimized = AutomatonOperations.minimize(nfa);
+		
+		assertEquals(getAutomaton4(), minimized);
+	}
+	
+	private NFA getAutomaton4() {
+		State state1 = new State();
+		State state2 = new State();
+		State state3 = new State();
+		State state4 = new State();
+		State state5 = new State(true);
+		state4.addTransition(new Transition(49, 51, state5));
+		state3.addTransition(new Transition(50, 50, state4));
+		State state6 = new State();
+		state6.addTransition(new Transition(49, 49, state5));
+		state3.addTransition(new Transition(51, 51, state6));
+		state3.addTransition(new Transition(49, 49, state4));
+		state2.addTransition(new Transition(49, 51, state3));
+		state1.addTransition(new Transition(50, 50, state2));
+		state1.addTransition(new Transition(49, 49, state2));
+		state1.addTransition(new Transition(51, 51, state2));
+		return new NFA(state1);
+	}
 }
