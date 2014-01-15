47243901746d76759a271137284f961355a3e9af
Ali Afroozeh <afroozeh@gmail.com>
2014-01-15 16:43:17 +0100

    Another test case for left factorization added.

--- test/org/jgll/grammar/leftfactorization/LeftFactorizationTest.java
+++ test/org/jgll/grammar/leftfactorization/LeftFactorizationTest.java
@@ -1,8 +1,14 @@
 package org.jgll.grammar.leftfactorization;
 
+
+import static org.jgll.util.CollectionsUtil.*;
+
 import org.jgll.grammar.Grammar;
 import org.jgll.grammar.GrammarBank;
 import org.jgll.grammar.GrammarBuilder;
+import org.jgll.grammar.symbol.Nonterminal;
+import org.jgll.grammar.symbol.Rule;
+import org.jgll.grammar.symbol.Character;
 import org.jgll.parser.GLLParser;
 import org.jgll.parser.ParseError;
 import org.jgll.parser.ParserFactory;
@@ -21,9 +27,30 @@ public class LeftFactorizationTest {
 		
 		Grammar grammar = builder.build();
 		
-		System.out.println(grammar);
-
-		GLLParser rdParser = ParserFactory.createRecursiveDescentParser(grammar);
-		NonterminalSymbolNode sppf = rdParser.parse(Input.fromString("a+a*a"), grammar, "E");
+		GLLParser parser = ParserFactory.createRecursiveDescentParser(grammar);
+		NonterminalSymbolNode sppf = parser.parse(Input.fromString("a+a*a"), grammar, "E");
 	}
+	
+	@Test
+	public void test2() throws ParseError {
+		Nonterminal S = new Nonterminal("S");
+		Nonterminal A = new Nonterminal("A");
+		Nonterminal B = new Nonterminal("B");
+		Character a = new Character('a');
+		Character b = new Character('b');
+		Character c = new Character('c');
+		
+		GrammarBuilder builder = new GrammarBuilder("test");
+		
+		builder.addRule(new Rule(S, list(A, b)));
+		builder.addRule(new Rule(S, list(A, c)));
+		builder.addRule(new Rule(A, list(B)));
+		builder.addRule(new Rule(B, list(a)));
+		
+		Grammar grammar = builder.build();
+		
+		GLLParser parser = ParserFactory.createRecursiveDescentParser(grammar);
+		NonterminalSymbolNode sppf = parser.parse(Input.fromString("ab"), grammar, "S");		
+	}
+	
 }
