ce09cbb3e34aa7a8724d1a6586efd4691e590173
Ali Afroozeh <afroozeh@gmail.com>
2014-05-22 00:49:52 +0200

    Working on original gss model

---
+++ src/org/jgll/parser/gss/NewGSSEdgeImpl.java
@@ -0,0 +1,64 @@
+package org.jgll.parser.gss;
+
+import org.jgll.grammar.slot.BodyGrammarSlot;
+import org.jgll.parser.HashFunctions;
+import org.jgll.sppf.SPPFNode;
+
+public class NewGSSEdgeImpl implements GSSEdge {
+	
+	private BodyGrammarSlot returnSlot;
+	private SPPFNode node;
+	private GSSNode destination;
+
+	private final int hash;
+
+	public NewGSSEdgeImpl(BodyGrammarSlot slot, SPPFNode node, GSSNode destination) {
+		this.returnSlot = slot;
+		this.node = node;
+		this.destination = destination;
+		this.hash = HashFunctions.defaulFunction().hash(returnSlot.getId(), 
+														node.getId(),
+														destination.getInputIndex(), 
+														destination.getGrammarSlot().getId());
+	}
+
+	public SPPFNode getNode() {
+		return node;
+	}
+
+	public BodyGrammarSlot getReturnSlot() {
+		return returnSlot;
+	}
+
+	public GSSNode getDestination() {
+		return destination;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof GSSEdge)) {
+			return false;
+		}
+
+		GSSEdge other = (GSSEdge) obj;
+
+		// Because destination.getInputIndex() == node.getLeftExtent, and
+		//         node.getRightExtent() == source.getLeftExtent 
+		// we don't use them here.
+		return 	returnSlot == other.getReturnSlot()
+				&& node.getId() == other.getNode().getId()
+				&& destination.getInputIndex() == other.getDestination().getInputIndex()
+				&& destination.getGrammarSlot() == other.getDestination().getGrammarSlot();
+	}
+
+	@Override
+	public int hashCode() {
+		return hash;
+	}
+
+}
---
+++ src/org/jgll/parser/gss/OriginalGSSEdgeImpl.java
@@ -0,0 +1,61 @@
+package org.jgll.parser.gss;
+
+import org.jgll.grammar.slot.BodyGrammarSlot;
+import org.jgll.parser.HashFunctions;
+import org.jgll.sppf.SPPFNode;
+
+public class OriginalGSSEdgeImpl implements GSSEdge {
+	
+	private SPPFNode node;
+	private GSSNode destination;
+
+	private final int hash;
+
+	public OriginalGSSEdgeImpl(SPPFNode node, GSSNode destination) {
+		this.node = node;
+		this.destination = destination;
+		this.hash = HashFunctions.defaulFunction().hash(node.getId(),
+														destination.getInputIndex(), 
+														destination.getGrammarSlot().getId());
+	}
+
+	public SPPFNode getNode() {
+		return node;
+	}
+
+	@Override
+	public BodyGrammarSlot getReturnSlot() {
+		throw new UnsupportedOperationException();
+	}
+
+	public GSSNode getDestination() {
+		return destination;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof GSSEdge)) {
+			return false;
+		}
+
+		GSSEdge other = (GSSEdge) obj;
+
+		// Because destination.getInputIndex() == node.getLeftExtent, and
+		//         node.getRightExtent() == source.getLeftExtent 
+		// we don't use them here.
+		return 	node.getId() == other.getNode().getId()
+				&& destination.getInputIndex() == other.getDestination().getInputIndex()
+				&& destination.getGrammarSlot() == other.getDestination().getGrammarSlot();
+	}
+
+	@Override
+	public int hashCode() {
+		return hash;
+	}
+
+}
