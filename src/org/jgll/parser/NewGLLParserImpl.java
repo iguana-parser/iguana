45c94819ad5fd34860b360013f6164adb04595be
Ali Afroozeh <afroozeh@gmail.com>
2014-05-21 20:56:08 +0200

    Remaining changes.

---
+++ src/org/jgll/parser/NewGLLParserImpl.java
@@ -0,0 +1,163 @@
+package org.jgll.parser;
+
+
+import org.jgll.grammar.GrammarGraph;
+import org.jgll.grammar.slot.BodyGrammarSlot;
+import org.jgll.grammar.slot.HeadGrammarSlot;
+import org.jgll.grammar.slot.LastGrammarSlot;
+import org.jgll.lexer.GLLLexerImpl;
+import org.jgll.parser.descriptor.Descriptor;
+import org.jgll.parser.descriptor.DescriptorFactory;
+import org.jgll.parser.gss.GSSEdge;
+import org.jgll.parser.gss.GSSNode;
+import org.jgll.parser.lookup.factory.DescriptorLookupFactory;
+import org.jgll.parser.lookup.factory.GSSLookupFactory;
+import org.jgll.parser.lookup.factory.SPPFLookupFactory;
+import org.jgll.sppf.NonPackedNode;
+import org.jgll.sppf.NonterminalSymbolNode;
+import org.jgll.sppf.SPPFNode;
+import org.jgll.util.Input;
+
+/**
+
+ * @author Ali Afroozeh
+ * 
+ */
+public class NewGLLParserImpl extends AbstractGLLParserImpl {
+		
+	public NewGLLParserImpl(GSSLookupFactory gssLookupFactory, 
+						 SPPFLookupFactory sppfLookupFactory, 
+						 DescriptorFactory descriptorFactory,
+						 DescriptorLookupFactory descriptorLookupFactory) {
+		super(gssLookupFactory, sppfLookupFactory, descriptorFactory, descriptorLookupFactory);
+	}
+	
+	@Override
+	public NonterminalSymbolNode parse(Input input, GrammarGraph grammar, String startSymbolName) throws ParseError {
+		return parse(new GLLLexerImpl(input, grammar), grammar, startSymbolName);
+	}
+	
+	public final void pop(GSSNode gssNode, int inputIndex, NonPackedNode node) {
+		
+		if (gssNode != u0) {
+
+			log.trace("Pop %s, %d, %s", gssNode, inputIndex, node);
+			
+			gssLookup.addToPoppedElements(gssNode, node);
+			
+			label:
+			for(GSSEdge edge : gssLookup.getEdges(gssNode)) {
+				BodyGrammarSlot returnSlot = edge.getReturnSlot();
+				
+				if(returnSlot.getPopConditions().execute(this, lexer, gssNode, inputIndex)) {
+					continue label;
+				}
+				
+				SPPFNode y = returnSlot.getNodeCreatorFromPop().create(this, returnSlot, edge.getNode(), node);
+				
+				// Perform a direct pop for continuations of the form A ::= alpha ., instead of 
+				// creating descriptors
+				GSSNode destinationGSS = edge.getDestination();
+				Descriptor descriptor = descriptorFactory.createDescriptor(returnSlot, destinationGSS, inputIndex, y);
+				
+				if (returnSlot instanceof LastGrammarSlot) {
+					if (descriptorLookup.addDescriptor(descriptor)) {
+						if (!returnSlot.getPopConditions().execute(this, lexer, destinationGSS, inputIndex)) {
+							HeadGrammarSlot head = (HeadGrammarSlot) destinationGSS.getGrammarSlot();
+							if (head.testFollowSet(lexer.getInput().charAt(inputIndex))) {
+								pop(destinationGSS, inputIndex, (NonPackedNode) y);
+							}
+						}
+					}
+				} else {
+					scheduleDescriptor(descriptor);					
+				}
+				
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * create(L, u, w) {
+     *	 let w be the value of cn
+	 *	 if there is not already a GSS node labelled (L,A ::= alpha . beta, ci) create one
+	 * 	 let v be the GSS node labelled (L,A ::= alpha . beta, ci)
+	 *   if there is not an edge from v to cu labelled w {
+	 * 		create an edge from v to cu labelled w
+	 * 		for all ((v, z) in P) {
+	 * 			let x be the node returned by getNodeP(A ::= alpha . beta, w, z)
+	 * 			add(L, cu, h, x)) where h is the right extent of z
+	 * 		}
+	 * 	 }
+	 * 	 return v
+	 * }
+	 * 
+	 * @param returnSlot the grammar label
+	 * 
+	 * @param nonterminalIndex the index of the nonterminal appearing as the head of the rule
+	 *                         where this position refers to. 
+	 * 
+	 * @param alternateIndex the index of the alternate of the rule where this position refers to.
+	 * 
+	 * @param position the position in the body of the rule where this position refers to
+	 *
+	 * @return 
+     *
+	 */
+	@Override
+	public final void createGSSNode(BodyGrammarSlot returnSlot, HeadGrammarSlot head) {
+		GSSNode v = gssLookup.getGSSNode(head, ci);
+		log.trace("GSSNode created: (%s, %d)",  head, ci);
+		createGSSEdge(returnSlot, cu, cn, v);
+		cu = v;
+	}
+	
+	@Override
+	public final boolean hasGSSNode(BodyGrammarSlot slot, HeadGrammarSlot head) {
+		GSSNode v = gssLookup.hasGSSNode(head, ci);
+		if(v == null) return false;
+		
+		log.trace("GSSNode found: (%s, %d)",  head, ci);
+		createGSSEdge(slot, cu, cn, v);
+		return true;
+	}
+	
+	protected void createGSSEdge(BodyGrammarSlot returnSlot, GSSNode destination, SPPFNode w, GSSNode source) {
+		if(gssLookup.getGSSEdge(source, destination, w, returnSlot)) {
+			
+			log.trace("GSS Edge created: %s from %s to %s", returnSlot, source, destination);
+			
+			label:
+			for (SPPFNode z : source.getPoppedElements()) {
+				
+				// Execute pop actions for continuations, when the GSS node already
+				// exits. The input index will be the right extend of the node
+				// stored in the popped elements.
+				if(returnSlot.getPopConditions().execute(this, lexer, destination, z.getRightExtent())) {
+					continue label;
+				}
+				
+				SPPFNode x = returnSlot.getNodeCreatorFromPop().create(this, returnSlot, w, z); 
+				
+				Descriptor descriptor = descriptorFactory.createDescriptor(returnSlot, destination, z.getRightExtent(), x);
+				
+				// Perform a direct pop for continuations of the form A ::= alpha ., instead of 
+				// creating descriptors
+				int newInputIndex = z.getRightExtent();
+				if (returnSlot instanceof LastGrammarSlot) {
+					if (descriptorLookup.addDescriptor(descriptor)) {
+						if (!returnSlot.getPopConditions().execute(this, lexer, destination, newInputIndex)) {
+							HeadGrammarSlot head = (HeadGrammarSlot) destination.getGrammarSlot();
+							if (head.testFollowSet(lexer.getInput().charAt(newInputIndex))) {
+								pop(destination, newInputIndex, (NonPackedNode) x);
+							}
+						}
+					}
+				} else {
+					scheduleDescriptor(descriptor);					
+				}
+			}
+		}
+	}	
+}
---
+++ src/org/jgll/parser/descriptor/factory/DescriptorFactory2.java
@@ -0,0 +1,35 @@
+package org.jgll.parser.descriptor.factory;
+
+import org.jgll.grammar.slot.GrammarSlot;
+import org.jgll.parser.descriptor.Descriptor;
+import org.jgll.parser.descriptor.DescriptorFactory;
+import org.jgll.parser.descriptor.DescriptorImpl;
+import org.jgll.parser.gss.GSSNode;
+import org.jgll.sppf.SPPFNode;
+import org.jgll.util.hashing.ExternalHasher;
+import org.jgll.util.hashing.hashfunction.HashFunction;
+
+
+public class DescriptorFactory2 implements DescriptorFactory {
+
+	@Override
+	public Descriptor createDescriptor(GrammarSlot slot, GSSNode gssNode, int inputIndex, SPPFNode sppfNode) {
+		return new DescriptorImpl(new ExternalHasher<Descriptor>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public int hash(Descriptor descriptor, HashFunction f) {
+				return f.hash(descriptor.getSPPFNode().getId(), descriptor.getInputIndex());
+			}
+			
+			@Override
+			public boolean equals(Descriptor descriptor1, Descriptor descriptor2) {
+				return descriptor1.getSPPFNode().getId() == descriptor2.getSPPFNode().getId() &&
+					   descriptor1.getInputIndex() == descriptor2.getInputIndex();
+
+			}
+		}, slot, gssNode, inputIndex, sppfNode);
+	}
+
+}
