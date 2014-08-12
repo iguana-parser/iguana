389a23fb47fed123731eac026f01e084508b05a4
Ali Afroozeh <afroozeh@gmail.com>
2014-08-12 02:09:19 +0200

    Changing the sppf lookup to only use hashmaps instead of hashmap/arrays

--- src/org/jgll/parser/lookup/SPPFLookupImpl.java
+++ src/org/jgll/parser/lookup/SPPFLookupImpl.java
@@ -1,5 +1,8 @@
 package org.jgll.parser.lookup;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jgll.grammar.GrammarGraph;
 import org.jgll.grammar.slot.BodyGrammarSlot;
 import org.jgll.grammar.slot.HeadGrammarSlot;
@@ -9,25 +12,17 @@ import org.jgll.sppf.NonterminalNode;
 import org.jgll.sppf.SPPFNode;
 import org.jgll.sppf.TokenSymbolNode;
 import org.jgll.util.Input;
-import org.jgll.util.hashing.ExternalHasher;
-import org.jgll.util.hashing.HashTableFactory;
-import org.jgll.util.hashing.IguanaSet;
-import org.jgll.util.hashing.hashfunction.HashFunction;
 import org.jgll.util.logging.LoggerWrapper;
 
 public class SPPFLookupImpl implements SPPFLookup {
 	
 	private static final LoggerWrapper log = LoggerWrapper.getLogger(SPPFLookupImpl.class);
 	
-	private HashTableFactory factory;
-
-	private int tableSize = (int) Math.pow(2, 10);
-	
 	private final TokenSymbolNode[][] tokenSymbolNodes;
 	
-	private final IguanaSet<NonterminalNode>[] nonterminalNodes;
+	private final Map<NonterminalNode, NonterminalNode> nonterminalNodes;
 
-	private final IguanaSet<IntermediateNode>[] intermediateNodes;
+	private final Map<IntermediateNode, IntermediateNode> intermediateNodes;
 	
 	private int countPackedNodes;
 	
@@ -35,15 +30,13 @@ public class SPPFLookupImpl implements SPPFLookup {
 	
 	public SPPFLookupImpl(GrammarGraph grammar, Input input) {
 		long start = System.nanoTime();
-		nonterminalNodes = new IguanaSet[input.length()];
-		
-		intermediateNodes = new IguanaSet[input.length()];
+
+		nonterminalNodes = new HashMap<>();
+		intermediateNodes = new HashMap<>();
 
 		tokenSymbolNodes = new TokenSymbolNode[grammar.getCountTokens()][input.length()];
 		long end = System.nanoTime();
 		log.info("SPPF lookup initialization: %d ms", (end - start) / 1000_000);
-		
-		factory = HashTableFactory.getFactory();
 	}
 
 	@Override
@@ -64,100 +57,38 @@ public class SPPFLookupImpl implements SPPFLookup {
 	@Override
 	public NonterminalNode getNonterminalNode(HeadGrammarSlot grammarSlot, int leftExtent, int rightExtent) {
 		NonterminalNode key = grammarSlot.createSPPFNode(grammarSlot.getNodeId(), grammarSlot.getFirstSlots().length, leftExtent, rightExtent);
-
-		IguanaSet<NonterminalNode> set = nonterminalNodes[rightExtent];
-
-		if (set == null) {
-			
+		NonterminalNode value = nonterminalNodes.get(key);
+		if (value == null) {
+			value = key;
 			log.trace("Nonterminal node created: %s", key);
-			
-			set = factory.newHashSet(tableSize, new ExternalHasher<NonterminalNode>() {
-
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public int hash(NonterminalNode n, HashFunction f) {
-					return f.hash(n.getId(), n.getLeftExtent());
-				}
-
-				@Override
-				public boolean equals(NonterminalNode n1, NonterminalNode n2) {
-					return n1.getId() == n2.getId() &&
-						   n1.getLeftExtent() == n2.getLeftExtent();
-				}
-			});
-			nonterminalNodes[rightExtent] = set;
-			set.add(key);
-			return key;
+			nonterminalNodes.put(key, value);
 		}
-
-		NonterminalNode oldValue = set.add(key);
-		if (oldValue == null) {
-			log.trace("Nonterminal node created: %s", key);
-			oldValue = key;
-		}
-
-		return oldValue;
+		return value;
 	}
 
 	@Override
-	public NonterminalNode findNonterminalNode(HeadGrammarSlot grammarSlot, int leftExtent, int rightExtent) {
-		
-		IguanaSet<NonterminalNode> set = nonterminalNodes[rightExtent];
-
-		if (set == null) {
-			return null;
-		}
-
+	public NonterminalNode findNonterminalNode(HeadGrammarSlot grammarSlot, int leftExtent, int rightExtent) {		
 		NonterminalNode key = grammarSlot.createSPPFNode(grammarSlot.getNodeId(), grammarSlot.getFirstSlots().length, leftExtent, rightExtent);
-		return set.get(key);
+		return nonterminalNodes.get(key);
 	}
 
 	@Override
 	public IntermediateNode getIntermediateNode(BodyGrammarSlot grammarSlot, int leftExtent, int rightExtent) {
 		IntermediateNode key = new IntermediateNode(grammarSlot.getNodeId(), leftExtent, rightExtent);
-
-		IguanaSet<IntermediateNode> set = intermediateNodes[rightExtent];
-
-		if (set == null) {
-			set = factory.newHashSet(tableSize, new ExternalHasher<IntermediateNode>() {
-
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public int hash(IntermediateNode n, HashFunction f) {
-					return f.hash(n.getId(), n.getLeftExtent());
-				}
-
-				@Override
-				public boolean equals(IntermediateNode n1, IntermediateNode n2) {
-					return n1.getId() == n2.getId() &&
-						   n1.getLeftExtent() == n2.getLeftExtent();
-				}
-			});
-			intermediateNodes[rightExtent] = set;
-			set.add(key);
-			return key;
+		IntermediateNode value = intermediateNodes.get(key);
+		
+		if (value == null) {
+			value = key;
+			log.trace("Intermediate node created: %s", key);
+			intermediateNodes.put(key, value);
 		}
-
-		IntermediateNode oldValue = set.add(key);
-		if (oldValue == null) {
-			oldValue = key;
-		}
-
-		return oldValue;
+		return value;
 	}
 
 	@Override
 	public IntermediateNode findIntermediateNode(BodyGrammarSlot grammarSlot, int leftExtent, int rightExtent) {
-		IguanaSet<IntermediateNode> set = intermediateNodes[rightExtent];
-
-		if (set == null) {
-			return null;
-		}
-
 		IntermediateNode key = new IntermediateNode(grammarSlot.getId(), leftExtent, rightExtent);
-		return set.get(key);
+		return intermediateNodes.get(key);
 	}
 	
 	@Override
@@ -182,32 +113,17 @@ public class SPPFLookupImpl implements SPPFLookup {
 	
 	@Override
 	public NonterminalNode getStartSymbol(HeadGrammarSlot startSymbol, int inputSize) {
-		if (nonterminalNodes[inputSize - 1] == null) {
-			return null;
-		}
-		return nonterminalNodes[inputSize - 1].get(new NonterminalNode(startSymbol.getNodeId(), startSymbol.getFirstSlots().length, 0, inputSize - 1));
+		return nonterminalNodes.get(new NonterminalNode(startSymbol.getNodeId(), startSymbol.getFirstSlots().length, 0, inputSize - 1));
 	}
 
 	@Override
 	public int getNonterminalNodesCount() {
-		int count = 0;
-		for(IguanaSet<NonterminalNode> set : nonterminalNodes) {
-			if(set != null) {
-				count += set.size();
-			}
-		}
-		return count;
+		return nonterminalNodes.size();
 	}
 
 	@Override
 	public int getIntermediateNodesCount() {
-		int count = 0;
-		for(IguanaSet<IntermediateNode> set : intermediateNodes) {
-			if(set != null) {
-				count += set.size();
-			}
-		}
-		return count;
+		return intermediateNodes.size();
 	}
 
 	@Override
