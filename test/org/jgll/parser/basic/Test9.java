37ae0739c0fa845f638bb9fa25c93584b8b868b5
Ali Afroozeh <afroozeh@gmail.com>
2014-10-04 13:18:50 +0200

    Fixing test cases

--- test/org/jgll/parser/basic/Test9.java
+++ test/org/jgll/parser/basic/Test9.java
@@ -55,36 +55,41 @@ public class Test9 {
 		GLLParser parser = ParserFactory.newParser(grammar, input);
 		ParseResult result = parser.parse(input, grammar.toGrammarGraph(), "S");
 		assertTrue(result.isParseSuccess());
+		assertEquals(1, result.asParseSuccess().getParseStatistics().getCountAmbiguousNodes());
 		assertTrue(result.asParseSuccess().getRoot().deepEquals(getSPPF()));
 	}
 	
 	private SPPFNode getSPPF() {
 		SPPFNodeFactory factory = new SPPFNodeFactory(grammar.toGrammarGraph());
-		NonterminalNode node1 = factory.createNonterminalNode("S", 0, 2).init();
+		NonterminalNode node1 = factory.createNonterminalNode("S", 0, 0, 2).init();
 		PackedNode node2 = factory.createPackedNode("S ::= A A b .", 1, node1);
 		IntermediateNode node3 = factory.createIntermediateNode("S ::= A A . b", 0, 1).init();
 		PackedNode node4 = factory.createPackedNode("S ::= A A . b", 1, node3);
-		NonterminalNode node5 = factory.createNonterminalNode("A", 0, 1).init();
+		NonterminalNode node5 = factory.createNonterminalNode("A", 0, 0, 1).init();
 		PackedNode node6 = factory.createPackedNode("A ::= a .", 0, node5);
 		TokenSymbolNode node7 = factory.createTokenNode("a", 0, 1);
 		node6.addChild(node7);
 		node5.addChild(node6);
-		NonterminalNode node8 = factory.createNonterminalNode("A", 1, 1).init();
+		NonterminalNode node8 = factory.createNonterminalNode("A", 0, 1, 1).init();
 		PackedNode node9 = factory.createPackedNode("A ::= .", 1, node8);
+		TokenSymbolNode node10 = factory.createTokenNode("epsilon", 1, 0);
+		node9.addChild(node10);
 		node8.addChild(node9);
 		node4.addChild(node5);
 		node4.addChild(node8);
-		PackedNode node10 = factory.createPackedNode("S ::= A A . b", 0, node3);
-		NonterminalNode node11 = factory.createNonterminalNode("A", 0, 0).init();
-		PackedNode node12 = factory.createPackedNode("A ::= .", 0, node11);
+		PackedNode node11 = factory.createPackedNode("S ::= A A . b", 0, node3);
+		NonterminalNode node12 = factory.createNonterminalNode("A", 0, 0, 0).init();
+		PackedNode node13 = factory.createPackedNode("A ::= .", 0, node12);
+		TokenSymbolNode node14 = factory.createTokenNode("epsilon", 0, 0);
+		node13.addChild(node14);
+		node12.addChild(node13);
 		node11.addChild(node12);
-		node10.addChild(node11);
-		node10.addChild(node5);
+		node11.addChild(node5);
 		node3.addChild(node4);
-		node3.addChild(node10);
-		TokenSymbolNode node13 = factory.createTokenNode("b", 1, 1);
+		node3.addChild(node11);
+		TokenSymbolNode node15 = factory.createTokenNode("b", 1, 1);
 		node2.addChild(node3);
-		node2.addChild(node13);
+		node2.addChild(node15);
 		node1.addChild(node2);
 		return node1;
 	}
