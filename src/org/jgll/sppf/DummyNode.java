7efbeb578605e0f83176a36067286664a509292c
Ali Afroozeh <afroozeh@gmail.com>
2014-12-30 16:31:41 +0100

    Dummy node fixed

--- src/org/jgll/sppf/DummyNode.java
+++ src/org/jgll/sppf/DummyNode.java
@@ -1,20 +1,14 @@
 package org.jgll.sppf;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.jgll.grammar.slot.GrammarSlot;
-import org.jgll.traversal.SPPFVisitor;
 
 /**
  * 
  * @author Ali Afroozeh
  * 
- * TODO: try to replace it with null, doesn't much make sense
- *
  */
-public class DummyNode extends NonPackedNode {
-	
+public class DummyNode extends TerminalNode {
+
 	private DummyNode(GrammarSlot slot, int leftExtent, int rightExtent) {
 		super(slot, leftExtent, rightExtent);
 	}
@@ -23,49 +17,14 @@ public class DummyNode extends NonPackedNode {
 	
 	public static DummyNode getInstance() {
 		if(instance == null) {
-			instance = new DummyNode(null, 0, 0);
+			instance = new DummyNode(null, -1, -1);
 		}
 		return instance;
 	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		return obj instanceof DummyNode;
-	}
-	
-	@Override
-	public int hashCode() {
-		return 16769023;
-	}
-
-	@Override
-	public SPPFNode getChildAt(int index) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public List<PackedNode> getChildren() {
-		return Collections.emptyList();
-	}
-
-	@Override
-	public boolean isAmbiguous() {
-		return false;
-	}
-
-	@Override
-	public int childrenCount() {
-		return 0;
-	}
-
-	@Override
-	public GrammarSlot getGrammarSlot() {
-		return null;
-	}
-
-	@Override
-	public void accept(SPPFVisitor visitAction) {
-		throw new UnsupportedOperationException();
+	public String toString() {
+		return "$";
 	}
 
 }
