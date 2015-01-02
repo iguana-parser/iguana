35a4731e87ea87fe889094f651662294627040f5
Anastassija <A.Izmaylova@cwi.nl>
2015-01-02 21:13:54 +0100

    A new, data-dependent test has been added.

---
+++ test/org/jgll/parser/datadependent/Test1.java
@@ -0,0 +1,39 @@
+package org.jgll.parser.datadependent;
+
+import org.jgll.grammar.Grammar;
+import org.jgll.grammar.symbol.Nonterminal;
+import org.jgll.grammar.symbol.Character;
+import org.jgll.grammar.symbol.Rule;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Anastasia Izmaylova
+ * 
+ * A ::= 'a'
+ * S ::= x = l:A
+ *
+ */
+
+public class Test1 {
+	
+	private Grammar grammar;
+
+	@Before
+	public void init() {
+		Nonterminal A = Nonterminal.withName("A");
+		Nonterminal S = Nonterminal.withName("S");
+		
+		Rule r1 = Rule.builder(A).addSymbol(Character.from('a')).build();
+		Rule r2 = Rule.builder(S).addSymbol(Nonterminal.builder(A).setLabel("l").setVariable("x").build()).build();
+		
+		grammar = Grammar.builder().addRules(r1, r2).build();
+	}
+	
+	@Test
+	public void test() {
+		System.out.println(grammar);
+	}
+
+}
