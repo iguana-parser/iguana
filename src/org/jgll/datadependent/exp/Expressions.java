208d65a65a120a7d1c28d1710c135506d7563d4d
Anastassija <A.Izmaylova@cwi.nl>
2015-01-04 23:40:29 +0100

    Sketching expressions for data-dependent GLL parsing.

---
+++ src/org/jgll/datadependent/exp/Expressions.java
@@ -0,0 +1,39 @@
+package org.jgll.datadependent.exp;
+
+import org.jgll.datadependent.env.Environment;
+import org.jgll.datadependent.exp.Expression;
+
+public class Expressions {
+	
+	static public final Expression TRUE = Expression.Boolean.TRUE;
+	static public final Expression FALSE = Expression.Boolean.FALSE;
+	
+	static public Expression integer(java.lang.Integer value) {
+		return new Expression.Integer(value);
+	}
+	
+	static public Expression real(java.lang.Float value) {
+		return new Expression.Real(value);
+	}
+	
+	static public Expression string(java.lang.String value) {
+		return new Expression.String(value);
+	}
+	
+	static public Expression println(Expression... args) {
+		return new Expression.Call("", args) {
+	
+					public Object interpret(Environment env) {
+						Object[] arguments = interpretArguments(env);
+						for (Object argument : arguments) {
+							System.out.print(argument);
+							System.out.print("; ");
+						}
+						System.out.println();
+						return null;
+					}
+		};
+	}
+	
+
+}
