fcd6ddd90e31b5a74a9c275c36fdc60eacf7a473
Ali Afroozeh <afroozeh@gmail.com>
2015-01-06 01:14:01 +0100

    Working on test cases for different configs

--- src/org/jgll/util/Configurations.java
+++ src/org/jgll/util/Configurations.java
@@ -11,35 +11,45 @@ import java.util.List;
 public class Configurations {
 	
 	public static final List<Configuration> configurations = new ArrayList<>();
+	public static final List<Configuration> newConfigs = new ArrayList<>();
+	public static final List<Configuration> originalConfigs = new ArrayList<>();
+	
+	private static final Configuration config1 = Configuration.DEFAULT;
+	
+	private static final Configuration config2 = Configuration.builder()
+												.setGSSType(NEW)
+											    .setGSSLookupStrategy(DISTRIBUTED)
+											    .setGSSLookupImpl(HASH_MAP)
+											    .setSPPFLookupStrategy(DISTRIBUTED)
+											    .setSPPFLookupImpl(HASH_MAP)
+											    .build();
+
+	private static final Configuration config3 = Configuration.builder()
+												.setGSSType(ORIGINAL)
+												.setGSSLookupStrategy(DISTRIBUTED)
+												.setGSSLookupImpl(ARRAY)
+												.setSPPFLookupStrategy(DISTRIBUTED)
+												.setSPPFLookupImpl(HASH_MAP)
+												.build();	
+
+	private static final Configuration config4 = Configuration.builder()
+												.setGSSType(ORIGINAL)
+											    .setGSSLookupStrategy(DISTRIBUTED)
+											    .setGSSLookupImpl(HASH_MAP)
+											    .setSPPFLookupStrategy(DISTRIBUTED)
+											    .setSPPFLookupImpl(HASH_MAP)
+											    .build();
 	
 	static {
+		configurations.add(config1);
+		configurations.add(config2);
+		configurations.add(config3);
+		configurations.add(config4);
 		
-		configurations.add(Configuration.DEFAULT);
+		newConfigs.add(config1);
+		newConfigs.add(config2);
 		
-		configurations.add(Configuration.builder()
-				.setGSSType(NEW)
-			    .setGSSLookupStrategy(DISTRIBUTED)
-			    .setGSSLookupImpl(HASH_MAP)
-			    .setSPPFLookupStrategy(DISTRIBUTED)
-			    .setSPPFLookupImpl(HASH_MAP)
-			    .build());
-
-//		configurations.add(Configuration.builder()
-//				.setGSSType(ORIGINAL)
-//				.setGSSLookupStrategy(DISTRIBUTED)
-//				.setGSSLookupImpl(ARRAY)
-//				.setSPPFLookupStrategy(DISTRIBUTED)
-//				.setSPPFLookupImpl(HASH_MAP)
-//				.build());
-//
-//		configurations.add(Configuration.builder()
-//				.setGSSType(ORIGINAL)
-//			    .setGSSLookupStrategy(DISTRIBUTED)
-//			    .setGSSLookupImpl(HASH_MAP)
-//			    .setSPPFLookupStrategy(DISTRIBUTED)
-//			    .setSPPFLookupImpl(HASH_MAP)
-//			    .build());
-	
+		originalConfigs.add(config3);
+		originalConfigs.add(config4);
 	}
-		
 }
