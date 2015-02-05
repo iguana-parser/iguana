c518cc4f76d8a58c17845c66b23b8dc761d83e0c
Anastassija <A.Izmaylova@cwi.nl>
2015-02-05 01:15:17 +0100

    Working on operator precedence (hard test case).

---
+++ test/org/jgll/parser/datadependent/Test8.java
@@ -0,0 +1,91 @@
+package org.jgll.parser.datadependent;
+
+import static org.jgll.datadependent.ast.AST.greaterEq;
+import static org.jgll.datadependent.ast.AST.integer;
+import static org.jgll.datadependent.ast.AST.var;
+import static org.jgll.grammar.condition.DataDependentCondition.predicate;
+
+import org.jgll.grammar.Grammar;
+import org.jgll.grammar.GrammarGraph;
+import org.jgll.grammar.symbol.Character;
+import org.jgll.grammar.symbol.Nonterminal;
+import org.jgll.grammar.symbol.Rule;
+import org.jgll.parser.GLLParser;
+import org.jgll.parser.ParseResult;
+import org.jgll.parser.ParserFactory;
+import org.jgll.util.Configuration;
+import org.jgll.util.Input;
+import org.jgll.util.Visualization;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Anastasia Izmaylova
+ * 
+ * S ::= E(0,0)
+ * 
+ * E(l,r) ::=[4 >= r] E(4,r) z // propagate r as a deep case: x ( ( a w ) z )
+ *         | [3 >= l] x E(0,3) // can be affected by left E  => constraint on l
+ *         | [2 >= r] E(0,0) w // can be affected by right E => constraint on r
+ *         | a
+ *
+ */
+
+public class Test8 {
+	
+	private Grammar grammar;
+
+	@Before
+	public void init() {
+		
+		Nonterminal S = Nonterminal.withName("S");
+		
+		Nonterminal E = Nonterminal.builder("E").addParameters("l", "r").build();
+		
+		Character z = Character.from('z');
+		Character w = Character.from('w');
+		
+		Rule r0 = Rule.withHead(S).addSymbol(Nonterminal.builder(E).apply(integer(0), integer(0)).build()).build();
+		
+		Rule r1_1 = Rule.withHead(E)
+					.addSymbol(Nonterminal.builder(E).apply(integer(4), var("r"))
+						.addPreCondition(predicate(greaterEq(integer(4), var("r")))).build())
+					.addSymbol(z).build();
+		
+		Rule r1_2 = Rule.withHead(E)
+					.addSymbol(Character.builder('x')
+							.addPreCondition(predicate(greaterEq(integer(3), var("l")))).build())
+					.addSymbol(Nonterminal.builder(E).apply(integer(0), integer(3)).build()).build();
+		
+		Rule r1_3 = Rule.withHead(E)
+					.addSymbol(Nonterminal.builder(E).apply(integer(0), integer(0))
+						.addPreCondition(predicate(greaterEq(integer(2), var("r")))).build())
+					.addSymbol(w).build();
+		
+		Rule r1_4 = Rule.withHead(E).addSymbol(Character.from('a')).build();
+		
+		grammar = Grammar.builder().addRules(r0, r1_1, r1_2, r1_3, r1_4).build();
+		
+	}
+	
+	@Test
+	public void test() {
+		System.out.println(grammar);
+		
+		Input input = Input.fromString("xawz");
+		GrammarGraph graph = grammar.toGrammarGraph(input, Configuration.DEFAULT);
+		
+		GLLParser parser = ParserFactory.getParser(Configuration.DEFAULT, input, grammar);
+		ParseResult result = parser.parse(input, graph, Nonterminal.withName("S"));
+		
+		Visualization.generateGrammarGraph("/Users/anastasiaizmaylova/git/diguana/test/org/jgll/parser/datadependent/", graph);
+		
+		if (result.isParseSuccess()) {
+			Visualization.generateSPPFGraph("/Users/anastasiaizmaylova/git/diguana/test/org/jgll/parser/datadependent/", 
+					result.asParseSuccess().getRoot(), parser.getRegistry(), input);
+		}
+		
+	}
+
+}
