9dcb1450c75093b623932544fdb2385d14481ba4
Ali Afroozeh <afroozeh@gmail.com>
2015-02-09 14:59:55 +0100

    Fix

--- test/org/jgll/regex/OptTest.java
+++ test/org/jgll/regex/OptTest.java
@@ -15,10 +15,23 @@ public class OptTest {
 	public void test1() {
 		RegularExpression regex = Opt.from(Character.from('a'));
 		Automaton automaton = regex.getAutomaton();
-		assertEquals(4, automaton.getCountStates());
+		assertEquals(2, automaton.getCountStates());
 
 		Matcher matcher = MatcherFactory.getMatcher(regex);
 		assertTrue(matcher.match(Input.fromString("a")));
+		assertEquals(0, matcher.match(Input.fromString(""), 0));
+	}
+	
+	@Test
+	public void test2() {
+		RegularExpression regex = Opt.from(Sequence.from("integer"));
+		Automaton automaton = regex.getAutomaton();
+		assertEquals(8, automaton.getCountStates());
+
+		Matcher matcher = MatcherFactory.getMatcher(regex);
+		assertTrue(matcher.match(Input.fromString("integer")));
+		assertFalse(matcher.match(Input.fromString("int")));
+		assertTrue(matcher.matchPrefix(Input.fromString("int")));
 		assertEquals(0, matcher.match(Input.fromString(""), 0));
 	}
 
