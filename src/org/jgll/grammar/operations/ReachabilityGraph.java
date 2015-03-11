3705a99dab72f24bfdf3574a9fc3e4cbdc1a43f9
Anastassija <A.Izmaylova@cwi.nl>
2015-03-11 22:11:04 +0100

    Extended the reachability graph calculation.

--- src/org/jgll/grammar/operations/ReachabilityGraph.java
+++ src/org/jgll/grammar/operations/ReachabilityGraph.java
@@ -4,14 +4,28 @@ import java.util.List;
 import java.util.Set;
 
 import org.jgll.grammar.Grammar;
+import org.jgll.grammar.symbol.Align;
+import org.jgll.grammar.symbol.Block;
+import org.jgll.grammar.symbol.Character;
+import org.jgll.grammar.symbol.CharacterRange;
+import org.jgll.grammar.symbol.Code;
+import org.jgll.grammar.symbol.Conditional;
+import org.jgll.grammar.symbol.EOF;
+import org.jgll.grammar.symbol.Epsilon;
+import org.jgll.grammar.symbol.IfThen;
+import org.jgll.grammar.symbol.IfThenElse;
 import org.jgll.grammar.symbol.Nonterminal;
+import org.jgll.grammar.symbol.Offside;
 import org.jgll.grammar.symbol.Rule;
 import org.jgll.grammar.symbol.Symbol;
+import org.jgll.grammar.symbol.Terminal;
+import org.jgll.grammar.symbol.While;
 import org.jgll.regex.Alt;
 import org.jgll.regex.Opt;
 import org.jgll.regex.Plus;
 import org.jgll.regex.Sequence;
 import org.jgll.regex.Star;
+import org.jgll.traversal.ISymbolVisitor;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ListMultimap;
@@ -48,6 +62,8 @@ public class ReachabilityGraph {
 	 */
 	public Multimap<Nonterminal, Nonterminal> calculateReachabilityGraph() {
 		
+		Visitor visitor = new Visitor(reachabilityGraph);
+		
 		Set<Nonterminal> nonterminals = definitions.keySet();
 		
 		boolean changed = true;
@@ -57,56 +73,150 @@ public class ReachabilityGraph {
 			changed = false;
 			
 			for (Nonterminal head : nonterminals) {
-				reachabilityGraph.get(head);
+				
+				visitor.setHead(head);
 				
 				for (Rule rule : definitions.get(head)) {
 					
 					List<Symbol> alternate = rule.getBody();
 					
-					if (alternate == null) {
+					if (alternate == null)
 						continue;
-					}
 					
-					for (Symbol symbol : alternate) {
-						
-						if (symbol instanceof Nonterminal) {
-							changed = add(head, (Nonterminal) symbol, reachabilityGraph);
-						} 
-						else if (symbol instanceof Star) {
-							Star star = (Star) symbol;
-							if (star.getSymbol() instanceof Nonterminal)
-								changed = add(head, (Nonterminal) star.getSymbol(), reachabilityGraph);
-						} 
-						else if (symbol instanceof Plus) {
-							Plus plus = (Plus) symbol;
-							if (plus.getSymbol() instanceof Nonterminal)
-								changed = add(head, (Nonterminal) plus.getSymbol(), reachabilityGraph);
-						}
-						else if (symbol instanceof Sequence<?>) {
-							Sequence<?> seq = (Sequence<?>) symbol;
-							for (Symbol s : seq.getSymbols()) {
-								if (s instanceof Nonterminal)
-									changed = add(head, (Nonterminal)s, reachabilityGraph);
-							}
-						}
-						else if (symbol instanceof Opt) {
-							Opt opt = (Opt) symbol;
-							if (opt.getSymbol() instanceof Nonterminal)
-								changed = add(head, (Nonterminal) opt.getSymbol(), reachabilityGraph);
-						}
-						else if (symbol instanceof Alt) {
-							Alt<?> alt = (Alt<?>) symbol;
-							for (Symbol s : alt.getSymbols()) {
-								if (s instanceof Nonterminal)
-									changed = add(head, (Nonterminal) s, reachabilityGraph);
-							}
-						}
-					}
+					for (Symbol symbol : alternate)
+						changed = symbol.accept(visitor);
+					
 				}
 			}
 		}
 		
 		return reachabilityGraph;
+	}
+	
+	private static class Visitor implements ISymbolVisitor<Boolean> {
+		
+		private final SetMultimap<Nonterminal, Nonterminal> reachabilityGraph;
+		
+		private Nonterminal head;
+		
+		public Visitor(SetMultimap<Nonterminal, Nonterminal> reachabilityGraph) {
+			this.reachabilityGraph = reachabilityGraph;
+		}
+		
+		public void setHead(Nonterminal head) {
+			this.head = head;
+		}
+
+		@Override
+		public Boolean visit(Align symbol) {
+			return symbol.getSymbol().accept(this);
+		}
+
+		@Override
+		public Boolean visit(Block symbol) {
+			boolean changed = false;
+			for (Symbol s : symbol.getSymbols())
+				changed |= s.accept(this);
+			return changed;
+		}
+
+		@Override
+		public Boolean visit(Character symbol) {
+			return false;
+		}
+
+		@Override
+		public Boolean visit(CharacterRange symbol) {
+			return false;
+		}
+
+		@Override
+		public Boolean visit(Code symbol) {
+			return symbol.getSymbol().accept(this);
+		}
+
+		@Override
+		public Boolean visit(Conditional symbol) {
+			return symbol.getSymbol().accept(this);
+		}
+
+		@Override
+		public Boolean visit(EOF symbol) {
+			return false;
+		}
+
+		@Override
+		public Boolean visit(Epsilon symbol) {
+			return false;
+		}
+
+		@Override
+		public Boolean visit(IfThen symbol) {
+			return symbol.getThenPart().accept(this);
+		}
+
+		@Override
+		public Boolean visit(IfThenElse symbol) {
+			return symbol.getThenPart().accept(this) || symbol.getElsePart().accept(this);
+		}
+
+		@Override
+		public Boolean visit(Nonterminal symbol) {
+			return add(head, symbol, reachabilityGraph);
+		}
+
+		@Override
+		public Boolean visit(Offside symbol) {
+			return symbol.getSymbol().accept(this);
+		}
+
+		@Override
+		public Boolean visit(Terminal symbol) {
+			return false;
+		}
+
+		@Override
+		public Boolean visit(While symbol) {
+			return symbol.getBody().accept(this);
+		}
+
+		@Override
+		public <E extends Symbol> Boolean visit(Alt<E> symbol) {
+			boolean changed = false;
+			for (Symbol s : symbol.getSymbols())
+				changed |= s.accept(this);
+			return changed;
+		}
+
+		@Override
+		public Boolean visit(Opt symbol) {
+			return symbol.getSymbol().accept(this);
+		}
+
+		@Override
+		public Boolean visit(Plus symbol) {
+			boolean changed = symbol.getSymbol().accept(this);
+			for (Symbol sep : symbol.getSeparators())
+				changed |= sep.accept(this);
+			return changed;
+		}
+
+		@Override
+		public <E extends Symbol> Boolean visit(Sequence<E> symbol) {
+			boolean changed = false;
+			for (Symbol s : symbol.getSymbols())
+				changed |= s.accept(this);
+			return changed;
+		}
+
+		@Override
+		public Boolean visit(Star symbol) {
+			boolean changed = symbol.getSymbol().accept(this);
+			for (Symbol sep : symbol.getSeparators())
+				changed |= sep.accept(this);
+			return changed;
+		}
+
 	}
 	
 	private static boolean add(Nonterminal a, Nonterminal nonterminal, SetMultimap<Nonterminal, Nonterminal> reachabilityGraph) {
