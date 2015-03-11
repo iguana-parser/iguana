75b5c9c61530245873337d33f265358d67e3bef8
Anastassija <A.Izmaylova@cwi.nl>
2015-03-11 05:38:31 +0100

    Re-generated precedence tests and added more.

--- test/org/jgll/benchmark/BenchmarkJava.java
+++ test/org/jgll/benchmark/BenchmarkJava.java
@@ -1,8 +1,11 @@
 package org.jgll.benchmark;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
 import org.jgll.grammar.Grammar;
+import org.jgll.grammar.GrammarGraph;
 import org.jgll.grammar.JavaCharacterLevel;
 import org.jgll.grammar.JavaNaturalCharacterLevel;
 import org.jgll.grammar.precedence.OperatorPrecedence;
@@ -11,21 +14,38 @@ import org.jgll.grammar.symbol.Start;
 import org.jgll.grammar.transformation.DesugarPrecedenceAndAssociativity;
 import org.jgll.grammar.transformation.EBNFToBNF;
 import org.jgll.grammar.transformation.LayoutWeaver;
+import org.jgll.parser.GLLParser;
+import org.jgll.parser.ParseResult;
+import org.jgll.parser.ParserFactory;
 import org.jgll.parser.datadependent.java.Java;
+import org.jgll.util.Configuration;
 import org.jgll.util.Input;
 
 public class BenchmarkJava {
 	
 	private static String sourceDir = "/Users/aliafroozeh/corpus/Java/jdk1.7.0_60-b19";
 //	private static Grammar grammar = new LayoutWeaver().transform(new EBNFToBNF().transform(JavaCharacterLevel.grammar));
-	private static Nonterminal start = Start.from(Nonterminal.withName("CompilationUnit"));
+	private static Nonterminal start = Start.from(Nonterminal.withName("VariableInitializer"));
 	
 //	private static Grammar grammar2 = new LayoutWeaver().transform(new OperatorPrecedence(JavaNaturalCharacterLevel.precedencePatterns(), JavaNaturalCharacterLevel.exceptPatterns()).transform(new EBNFToBNF().transform(JavaNaturalCharacterLevel.grammar)));
 	
 	private static Grammar grammar = new DesugarPrecedenceAndAssociativity().transform(new LayoutWeaver().transform(new EBNFToBNF().transform(Java.grammar)));
 	
 	public static void main(String[] args) throws IOException {
-		IguanaBenchmark.builder(grammar, start).addFile("/Users/aliafroozeh/test.java").setRunCount(100).build().run();
+		
+		System.out.println(grammar);
+		
+		Input input = Input.fromString("1+2*3");
+        GrammarGraph graph = grammar.toGrammarGraph(input, Configuration.DEFAULT);
+
+        // Visualization.generateGrammarGraph("/Users/anastasiaizmaylova/git/diguana/test/org/jgll/parser/datadependent/excepts/", graph);
+
+//        GLLParser parser = ParserFactory.getParser(Configuration.DEFAULT, input, grammar);
+//        ParseResult result = parser.parse(input, graph, start);
+//        
+//        System.out.println(result.toString());
+		
+//		IguanaBenchmark.builder(grammar, start).addFile("/Users/anastasiaizmaylova/git/diguana/test.java").setRunCount(5).build().run();
 //		IguanaBenchmark.builder(grammar, start).addDirectory(sourceDir, "java", true).build().run();
 	}
 }
