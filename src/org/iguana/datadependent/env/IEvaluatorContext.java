57c79d78a8274513dbbfd59c2215e0f79c4c3e81
Ali Afroozeh <afroozeh@gmail.com>
2015-04-26 21:21:22 +0200

    change

--- src/org/iguana/datadependent/env/IEvaluatorContext.java
+++ src/org/iguana/datadependent/env/IEvaluatorContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, CWI
+ * Copyright (c) 2015, Ali Afroozeh and Anastasia Izmaylova, Centrum Wiskunde & Informatica (CWI)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
