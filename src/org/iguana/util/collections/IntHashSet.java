a5cc0c7371bfaba8250f90cd06f645062e40a8e1
Jurgen J. Vinju <jurgen@vinju.org>
2015-06-18 13:30:40 +0200

    removed broken import

--- src/org/iguana/util/collections/IntHashSet.java
+++ src/org/iguana/util/collections/IntHashSet.java
@@ -11,8 +11,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 /**
  * 
  * @author Ali Afroozeh
