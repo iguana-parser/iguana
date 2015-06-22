69bedcbcb90296d945f65dd4f57c88b1c8248294
Ali Afroozeh <afroozeh@gmail.com>
2015-06-22 17:50:19 +0200

    Rewrote lambda to annonymous inner class to avoid weird deserialization problem. Should retry when Eclipse Mars is released.

--- src/org/iguana/util/SemanticAction.java
+++ src/org/iguana/util/SemanticAction.java
@@ -6,13 +6,22 @@ import java.util.function.Consumer;
 @FunctionalInterface
 public interface SemanticAction extends Serializable {
 
-	public static final long serialVersionUID = 1L;
-	
 	public Object execute(Object o);
 	
 	public static SemanticAction Unit = x -> x;
 	
 	public static SemanticAction from(Consumer<Object> c) {
-		return t -> { c.accept(t); return null; };
+		// TODO: check it in Eclipse Mars, trhows Inavid lambda deserialization in Kepler.
+		return new SemanticAction() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Object execute(Object o) {
+				c.accept(o);
+				return null;
+			}
+		};
+		
 	}
 }
