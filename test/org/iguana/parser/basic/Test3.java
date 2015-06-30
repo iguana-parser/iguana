e96bd509891c73e0220e3cb1d5e6cd048b7cefad
Anastassija <A.Izmaylova@cwi.nl>
2015-06-30 16:22:29 +0200

    Updating tests with respect to the changes in the SPPF construction.

--- test/org/iguana/parser/basic/Test3.java
+++ test/org/iguana/parser/basic/Test3.java
@@ -45,6 +45,7 @@ import org.iguana.grammar.symbol.Rule;
 import org.iguana.parser.ParseResult;
 import org.iguana.parser.ParseSuccess;
 import org.iguana.parser.ParserFactory;
+import org.iguana.sppf.IntermediateNode;
 import org.iguana.sppf.NonterminalNode;
 import org.iguana.sppf.PackedNode;
 import org.iguana.sppf.SPPFNodeFactory;
@@ -114,20 +115,24 @@ public class Test3 extends AbstractParserTest {
 				.setGSSEdgesCount(0)
 				.setNonterminalNodesCount(1)
 				.setTerminalNodesCount(2)
-				.setIntermediateNodesCount(0)
-				.setPackedNodesCount(1)
+				.setIntermediateNodesCount(1)
+				.setPackedNodesCount(2)
 				.setAmbiguousNodesCount(0).build();
 		return new ParseSuccess(expectedSPPF(registry), statistics);
 	}
 	
 	private static NonterminalNode expectedSPPF(GrammarGraph registry) {
 		SPPFNodeFactory factory = new SPPFNodeFactory(registry);
-		NonterminalNode node1 = factory.createNonterminalNode("A", 0, 2);
-		PackedNode node2 = factory.createPackedNode("A ::= a b .", 1, node1);
-		TerminalNode node3 = factory.createTerminalNode("a", 0, 1);
-		TerminalNode node4 = factory.createTerminalNode("b", 1, 2);
+		NonterminalNode node1 = factory.createNonterminalNode("A", 0, 0, 2);
+		PackedNode node2 = factory.createPackedNode("A ::= a b .", 2, node1);
+		IntermediateNode node3 = factory.createIntermediateNode("A ::= a b .", 0, 2);
+		PackedNode node4 = factory.createPackedNode("A ::= a b .", 1, node3);
+		TerminalNode node5 = factory.createTerminalNode("a", 0, 1);
+		TerminalNode node6 = factory.createTerminalNode("b", 1, 2);
+		node4.addChild(node5);
+		node4.addChild(node6);
+		node3.addChild(node4);
 		node2.addChild(node3);
-		node2.addChild(node4);
 		node1.addChild(node2);
 		return node1;
 	}
