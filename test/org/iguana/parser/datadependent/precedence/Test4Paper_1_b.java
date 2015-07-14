e6c970b60616ceb56516b26c3b127b2a2a86f186
Anastassija <A.Izmaylova@cwi.nl>
2015-07-14 16:17:15 +0200

    More operator-precedence tests.

---
+++ test/org/iguana/parser/datadependent/precedence/Test4Paper_1_b.java
@@ -0,0 +1,81 @@
+package org.iguana.parser.datadependent.precedence;
+
+import java.util.Arrays;
+import org.iguana.datadependent.ast.AST;
+import org.iguana.grammar.Grammar;
+import org.iguana.grammar.GrammarGraph;
+import org.iguana.grammar.condition.ConditionType;
+import org.iguana.grammar.condition.RegularExpressionCondition;
+import org.iguana.grammar.symbol.*;
+import org.iguana.grammar.symbol.Character;
+import static org.iguana.grammar.symbol.LayoutStrategy.*;
+import org.iguana.grammar.transformation.DesugarAlignAndOffside;
+import org.iguana.grammar.transformation.DesugarPrecedenceAndAssociativity;
+import org.iguana.grammar.transformation.DesugarState;
+import org.iguana.grammar.transformation.EBNFToBNF;
+import org.iguana.grammar.transformation.LayoutWeaver;
+import org.iguana.parser.GLLParser;
+import org.iguana.parser.ParseResult;
+import org.iguana.parser.ParserFactory;
+import org.iguana.regex.*;
+import org.iguana.util.Configuration;
+import org.iguana.util.Input;
+import org.iguana.util.Visualization;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Sets;
+import static org.iguana.util.CollectionsUtil.*;
+
+@SuppressWarnings("unused")
+public class Test4Paper_1_b {
+
+    @Test
+    public void test() {
+         Grammar grammar =
+
+Grammar.builder()
+.setLayout(Nonterminal.builder("L").build())
+// $default$ ::=  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("$default$").build()).setLayoutStrategy(NO_LAYOUT).setRecursion(Recursion.NON_REC).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,-1,false,-1)).build())
+// L ::= (\u0009-\\u000A | \\u000D | \u0020)*  !>>  (\u0009-\\u000A | \\u000D | \u0020)  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("L").build()).addSymbol(Star.builder(Alt.builder(CharacterRange.builder(9, 10).build(), CharacterRange.builder(13, 13).build(), CharacterRange.builder(32, 32).build()).build()).addPostConditions(set(new RegularExpressionCondition(ConditionType.NOT_FOLLOW, Alt.builder(CharacterRange.builder(9, 10).build(), CharacterRange.builder(13, 13).build(), CharacterRange.builder(32, 32).build()).build()))).build()).setLayoutStrategy(NO_LAYOUT).setRecursion(Recursion.NON_REC).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,-1,false,-1)).build())
+// E ::= (i f) E (t h e n) E (e l s e) E  {UNDEFINED,1,RIGHT_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(105).build(), Character.builder(102).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(116).build(), Character.builder(104).build(), Character.builder(101).build(), Character.builder(110).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(101).build(), Character.builder(108).build(), Character.builder(115).build(), Character.builder(101).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.RIGHT_REC).setAssociativity(Associativity.UNDEFINED).setPrecedence(1).setPrecedenceLevel(PrecedenceLevel.from(1,1,1,true,false,false,-1,false,-1)).build())
+// E ::= (a)  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(97).build()).build()).build()).setRecursion(Recursion.NON_REC).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,1,true,false,false,-1,false,-1)).build())
+// E ::= E (+) E  {LEFT,2,LEFT_RIGHT_REC} PREC(2,2) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(43).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.LEFT_RIGHT_REC).setAssociativity(Associativity.LEFT).setPrecedence(2).setPrecedenceLevel(PrecedenceLevel.from(2,2,-1,false,false,true,1,false,-1)).build())
+// E ::= E (*) E  {LEFT,3,LEFT_RIGHT_REC} PREC(3,3) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(42).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.LEFT_RIGHT_REC).setAssociativity(Associativity.LEFT).setPrecedence(3).setPrecedenceLevel(PrecedenceLevel.from(3,3,-1,false,false,true,1,false,-1)).build())
+// E ::= (-) E  {UNDEFINED,4,RIGHT_REC} PREC(4,4) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(45).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.RIGHT_REC).setAssociativity(Associativity.UNDEFINED).setPrecedence(4).setPrecedenceLevel(PrecedenceLevel.from(4,4,4,true,false,true,1,false,-1)).build())
+// S ::= E  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("S").build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.NON_REC).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,-1,false,-1)).build())
+.build();
+
+         grammar = new EBNFToBNF().transform(grammar);
+         // System.out.println(grammar);
+
+         grammar = new DesugarPrecedenceAndAssociativity().transform(grammar);
+         System.out.println(grammar.toStringWithOrderByPrecedence());
+
+         grammar = new LayoutWeaver().transform(grammar);
+
+         Input input = Input.fromString("a + if a then a else a + a");
+         GrammarGraph graph = grammar.toGrammarGraph(input, Configuration.DEFAULT);
+
+         // Visualization.generateGrammarGraph("/Users/anastasiaizmaylova/git/iguana/test/org/iguana/parser/datadependent/precedence/", graph);
+
+         GLLParser parser = ParserFactory.getParser(Configuration.DEFAULT, input, grammar);
+         ParseResult result = parser.parse(input, graph, Nonterminal.withName("S"));
+
+         Assert.assertTrue(result.isParseSuccess());
+
+         // Visualization.generateSPPFGraph("/Users/anastasiaizmaylova/git/iguana/test/org/iguana/parser/datadependent/precedence/",
+         //                   result.asParseSuccess().getRoot(), input);
+
+         Assert.assertTrue(result.asParseSuccess().getStatistics().getCountAmbiguousNodes() == 0);
+    }
+}
