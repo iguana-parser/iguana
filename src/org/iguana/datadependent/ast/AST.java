363bfb318d67a51f56ad2a980d278458c536a21c
Anastassija <A.Izmaylova@cwi.nl>
2015-08-17 23:52:06 +0200

    Extended the expressions.

--- src/org/iguana/datadependent/ast/AST.java
+++ src/org/iguana/datadependent/ast/AST.java
@@ -731,6 +731,30 @@ public class AST {
 		};
 	}
 	
+	static public Expression get(Expression arg1, Expression arg2) {
+		return new Expression.Call("get", arg1, arg2) {
+			
+			private static final long serialVersionUID = 1L;
+
+					@Override
+					public Object interpret(IEvaluatorContext ctx) {
+						List<?> value = (List<?>) arg1.interpret(ctx);
+						int i = (java.lang.Integer) arg2.interpret(ctx);
+						return value.get(i);
+					}
+					
+					@Override
+					public java.lang.String getConstructorCode() {
+						return "AST.get(" + arg1.getConstructorCode() + "," + arg2.getConstructorCode() + ")";
+					}
+					
+					@Override
+					public java.lang.String toString() {
+						return java.lang.String.format("get(%s,%s)", arg1, arg2);
+					}
+		};
+	}
+	
 	static public Expression lShiftANDEqZero(Expression lhs, Expression rhs) {
 		return new Expression.LShiftANDEqZero(lhs, rhs);
 	}
