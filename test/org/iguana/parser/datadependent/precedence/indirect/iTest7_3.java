a13521fdb82e92ac62d8720e361f4b2ba0ff12b6
Anastassija <A.Izmaylova@cwi.nl>
2015-09-05 19:50:06 +0200

    Testing.

---
+++ test/org/iguana/parser/datadependent/precedence/indirect/iTest7_3.java
@@ -0,0 +1,82 @@
+package org.iguana.parser.datadependent.precedence.indirect;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import org.iguana.datadependent.ast.AST;
+import org.iguana.grammar.Grammar;
+import org.iguana.grammar.GrammarGraph;
+import org.iguana.grammar.condition.ConditionType;
+import org.iguana.grammar.condition.RegularExpressionCondition;
+import org.iguana.grammar.symbol.*;
+import org.iguana.grammar.symbol.Character;
+import static org.iguana.grammar.symbol.LayoutStrategy.*;
+import org.iguana.grammar.transformation.DesugarAlignAndOffside;
+import org.iguana.grammar.transformation.DesugarPrecedenceAndAssociativity;
+import org.iguana.grammar.transformation.DesugarState;
+import org.iguana.grammar.transformation.EBNFToBNF;
+import org.iguana.grammar.transformation.LayoutWeaver;
+import org.iguana.parser.GLLParser;
+import org.iguana.parser.ParseResult;
+import org.iguana.parser.ParserFactory;
+import org.iguana.regex.*;
+import org.iguana.util.Configuration;
+import org.iguana.util.Input;
+import org.iguana.util.Visualization;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Sets;
+import static org.iguana.util.CollectionsUtil.*;
+
+@SuppressWarnings("unused")
+public class iTest7_3 {
+
+    @Test
+    public void test() {
+         Grammar grammar =
+
+Grammar.builder()
+.addEBNFr("Arg+",new HashSet<String>(Arrays.asList("$E","$Arg","E","Arg")))
+// $default$ ::=  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("$default$").build()).setLayoutStrategy(NO_LAYOUT).setRecursion(Recursion.NON_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList())).setRightEnds(new HashSet<String>(Arrays.asList())).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,new Integer[]{},false,new Integer[]{})).build())
+// Arg ::= (b)  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("Arg").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(98).build()).build()).build()).setRecursion(Recursion.NON_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E","$Arg","E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","E","Arg+"))).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,1,false,false,false,new Integer[]{},false,new Integer[]{})).build())
+// Arg ::= E  {UNDEFINED,1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("Arg").build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.NON_REC).setiRecursion(Recursion.iRIGHT_REC).setLeftEnd("").setRightEnd("E").setLeftEnds(new HashSet<String>(Arrays.asList("$E","$Arg","E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","E","Arg+"))).setAssociativity(Associativity.UNDEFINED).setPrecedence(1).setPrecedenceLevel(PrecedenceLevel.from(1,1,1,false,false,false,new Integer[]{},false,new Integer[]{})).build())
+// E ::= E (+) E  {LEFT,1,LEFT_RIGHT_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(43).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.LEFT_RIGHT_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","Arg","Arg+"))).setAssociativity(Associativity.LEFT).setPrecedence(1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,new Integer[]{},false,new Integer[]{})).build())
+// E ::= (a)  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(97).build()).build()).build()).setRecursion(Recursion.NON_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","Arg","Arg+"))).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,new Integer[]{},false,new Integer[]{})).build())
+// E ::= (&) E  {NON_ASSOC,2,RIGHT_REC} NON_ASSOC(2,2,2) PREC(2,2) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(38).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.RIGHT_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","Arg","Arg+"))).setAssociativity(Associativity.NON_ASSOC).setPrecedence(2).setAssociativityGroup(new AssociativityGroup(Associativity.NON_ASSOC,PrecedenceLevel.from(2,2,-1,true,false,false,new Integer[]{},false,new Integer[]{}),2,2,2)).setPrecedenceLevel(PrecedenceLevel.from(2,2,-1,true,false,false,new Integer[]{},false,new Integer[]{})).build())
+// E ::= (*) E  {NON_ASSOC,2,RIGHT_REC} NON_ASSOC(2,2,2) PREC(2,2) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(42).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.RIGHT_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","Arg","Arg+"))).setAssociativity(Associativity.NON_ASSOC).setPrecedence(2).setAssociativityGroup(new AssociativityGroup(Associativity.NON_ASSOC,PrecedenceLevel.from(2,2,-1,true,false,false,new Integer[]{},false,new Integer[]{}),2,2,2)).setPrecedenceLevel(PrecedenceLevel.from(2,2,-1,true,false,false,new Integer[]{},false,new Integer[]{})).build())
+// E ::= E Arg+  {NON_ASSOC,2,LEFT_REC} NON_ASSOC(2,2,2) PREC(2,2) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Plus.builder(Nonterminal.builder("Arg").build()).build()).setRecursion(Recursion.LEFT_REC).setiRecursion(Recursion.iRIGHT_REC).setLeftEnd("").setRightEnd("Arg+").setLeftEnds(new HashSet<String>(Arrays.asList("$E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","Arg","Arg+"))).setAssociativity(Associativity.NON_ASSOC).setPrecedence(2).setAssociativityGroup(new AssociativityGroup(Associativity.NON_ASSOC,PrecedenceLevel.from(2,2,-1,true,false,false,new Integer[]{},false,new Integer[]{}),2,2,2)).setPrecedenceLevel(PrecedenceLevel.from(2,2,-1,true,false,false,new Integer[]{},false,new Integer[]{})).build())
+// E ::= E (^) E  {RIGHT,3,LEFT_RIGHT_REC} PREC(3,3) 
+.addRule(Rule.withHead(Nonterminal.builder("E").build()).addSymbol(Nonterminal.builder("E").build()).addSymbol(Terminal.builder(Sequence.builder(Character.builder(94).build()).build()).build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.LEFT_RIGHT_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","Arg","Arg+"))).setAssociativity(Associativity.RIGHT).setPrecedence(3).setPrecedenceLevel(PrecedenceLevel.from(3,3,-1,false,false,true,new Integer[]{2},false,new Integer[]{})).build())
+// S ::= E  {UNDEFINED,-1,NON_REC} PREC(1,1) 
+.addRule(Rule.withHead(Nonterminal.builder("S").build()).addSymbol(Nonterminal.builder("E").build()).setRecursion(Recursion.NON_REC).setiRecursion(Recursion.NON_REC).setLeftEnd("").setRightEnd("").setLeftEnds(new HashSet<String>(Arrays.asList("$E","E"))).setRightEnds(new HashSet<String>(Arrays.asList("$E","$Arg","E","Arg","Arg+"))).setAssociativity(Associativity.UNDEFINED).setPrecedence(-1).setPrecedenceLevel(PrecedenceLevel.from(1,1,-1,false,false,false,new Integer[]{},false,new Integer[]{})).build())
+.build();
+
+         grammar = new EBNFToBNF().transform(grammar);
+         // System.out.println(grammar);
+
+         DesugarPrecedenceAndAssociativity precedenceAndAssociativity = new DesugarPrecedenceAndAssociativity();
+         precedenceAndAssociativity.setOP2();
+         
+		 grammar = precedenceAndAssociativity.transform(grammar);
+         System.out.println(grammar.toString());
+
+         Input input = Input.fromString("a^a*a&a");
+         GrammarGraph graph = grammar.toGrammarGraph(input, Configuration.DEFAULT);
+
+         // Visualization.generateGrammarGraph("test/org/iguana/parser/datadependent/precedence/", graph);
+
+         GLLParser parser = ParserFactory.getParser(Configuration.DEFAULT, input, grammar);
+         ParseResult result = parser.parse(input, graph, Nonterminal.withName("S"));
+
+         Assert.assertTrue(result.isParseError());
+    }
+}
