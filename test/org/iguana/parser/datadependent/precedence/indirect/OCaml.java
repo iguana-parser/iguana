cfbbba34ccb8ee6d5963ff8e96655c73713de664
Anastassija <A.Izmaylova@cwi.nl>
2015-09-06 22:46:01 +0200

    The ultimate test using OCaml.

---
+++ test/org/iguana/parser/datadependent/precedence/indirect/OCaml.java
@@ -0,0 +1,57 @@
+package org.iguana.parser.datadependent.precedence.indirect;
+
+import java.io.File;
+
+import org.iguana.grammar.Grammar;
+import org.iguana.grammar.GrammarGraph;
+import org.iguana.grammar.symbol.Nonterminal;
+import org.iguana.grammar.transformation.DesugarPrecedenceAndAssociativity;
+import org.iguana.grammar.transformation.EBNFToBNF;
+import org.iguana.grammar.transformation.LayoutWeaver;
+import org.iguana.parser.GLLParser;
+import org.iguana.parser.ParseResult;
+import org.iguana.parser.ParserFactory;
+import org.iguana.util.Configuration;
+import org.iguana.util.Input;
+import org.iguana.util.Visualization;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OCaml {
+	
+	@Test
+    public void test() {
+		
+		Grammar grammar = Grammar.load(new File("test/org/iguana/parser/datadependent/precedence/indirect/OCaml"));
+		
+		grammar = new EBNFToBNF().transform(grammar);
+		
+		DesugarPrecedenceAndAssociativity precedence = new DesugarPrecedenceAndAssociativity();
+		precedence.setOP2();
+		
+		grammar = precedence.transform(grammar);
+		// System.out.println(grammar.toString());
+		
+		grammar = new LayoutWeaver().transform(grammar);
+		
+		Input input = Input.fromFile(new File("test/org/iguana/parser/datadependent/precedence/indirect/Test.ml"));
+		
+		GrammarGraph graph = grammar.toGrammarGraph(input, Configuration.DEFAULT);
+
+        // Visualization.generateGrammarGraph("test/org/iguana/parser/datadependent/precedence/indirect/", graph);
+
+        GLLParser parser = ParserFactory.getParser(Configuration.DEFAULT, input, grammar);
+        ParseResult result = parser.parse(input, graph, Nonterminal.withName("CompilationUnit"));
+
+        Assert.assertTrue(result.isParseSuccess());
+
+        // Visualization.generateSPPFGraph("test/org/iguana/parser/datadependent/precedence/indirect/",
+        //                   result.asParseSuccess().getRoot(), input);
+
+        Assert.assertEquals(0, result.asParseSuccess().getStatistics().getCountAmbiguousNodes());
+        
+        System.out.println(result.asParseSuccess().getStatistics());
+		
+	}
+
+}
