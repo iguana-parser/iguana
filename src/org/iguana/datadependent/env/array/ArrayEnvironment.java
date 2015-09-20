d4868670256c8553fe19ea7dda19a2aca7d8f5f4
Anastassija <A.Izmaylova@cwi.nl>
2015-09-20 03:47:40 +0200

    Working on an array-based environment implementation.

--- src/org/iguana/datadependent/env/array/ArrayEnvironment.java
+++ src/org/iguana/datadependent/env/array/ArrayEnvironment.java
@@ -5,12 +5,10 @@ import org.iguana.datadependent.env.Environment;
 public class ArrayEnvironment implements Environment {
 	
 	private final Object[] values;
-	private final int length;
 	private final int hashCode;
 	
-	private ArrayEnvironment(Object[] values, int length, int hashCode) {
+	private ArrayEnvironment(Object[] values, int hashCode) {
 		this.values = values;
-		this.length = length;
 		this.hashCode = hashCode;
 	}
 
@@ -51,37 +49,39 @@ public class ArrayEnvironment implements Environment {
 
 	@Override
 	public Environment _declare(Object value) {
-		Object[] values = new Object[this.values.length];
+		int length = this.values.length + 1;
+		Object[] values = new Object[length];
 		
-		System.arraycopy(values, 0, this.values, 0, length);
-		values[length] = value;
+		System.arraycopy(this.values, 0, values, 0, this.values.length);
+		values[this.values.length] = value;
 		
-		return new ArrayEnvironment(values, length + 1, hashCode + 31 * value.hashCode());
+		return new ArrayEnvironment(values, hashCode + 31 * value.hashCode());
 	}
 
 	@Override
 	public Environment declare(Object[] values) {
-		Object[] newvalues = new Object[this.values.length];
+		int length = this.values.length + values.length;
+		Object[] vals = new Object[length];
 		
-		System.arraycopy(newvalues, 0, this.values, 0, length);
+		System.arraycopy(this.values, 0, vals, 0, this.values.length);
 		int hashCode = this.hashCode;
 		
 		int j = 0;
-		for (int i = length; i < values.length; i++) {
-			newvalues[i] = values[j];
+		for (int i = this.values.length; i < values.length; i++) {
+			vals[i] = values[j];
 			hashCode = hashCode + 31 * values[j].hashCode();
 			j++;
 		}
 		
-		return new ArrayEnvironment(newvalues, length + values.length, hashCode);
+		return new ArrayEnvironment(vals, hashCode);
 	}
 
 	@Override
 	public Environment store(int i, Object value) {
-		Object[] values = new Object[length];
+		Object[] values = new Object[this.values.length];
 		
 		int hashCode = 0;
-		for (int j = 0; j < length; j++) {
+		for (int j = 0; j < values.length; j++) {
 			
 			if (i != j)
 				values[i] = this.values[i];
@@ -92,7 +92,7 @@ public class ArrayEnvironment implements Environment {
 				hashCode = hashCode + values[i].hashCode();
 		}
 		
-		return new ArrayEnvironment(values, hashCode, length);
+		return new ArrayEnvironment(values, hashCode);
 	}
 
 	@Override
