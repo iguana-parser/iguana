7bd4773f123f45599db0167c071c6eaf1c6264b2
Anastassija <A.Izmaylova@cwi.nl>
2015-12-01 15:49:19 +0100

    Developing iggy.

--- src/org/iguana/grammar/iggy/Builder.java
+++ src/org/iguana/grammar/iggy/Builder.java
@@ -27,18 +27,22 @@
 package org.iguana.grammar.iggy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.iguana.datadependent.ast.Expression;
 import org.iguana.grammar.Grammar;
-import org.iguana.grammar.symbol.Nonterminal;
-import org.iguana.grammar.symbol.Return;
-import org.iguana.grammar.symbol.Rule;
-import org.iguana.grammar.symbol.Symbol;
+import org.iguana.grammar.symbol.*;
+import org.iguana.regex.*;
 
 /**
  * @author Anastasia Izmaylova
  */
 public abstract class Builder implements iguana.parsetrees.iggy.Builder {
+
+    private Map<String, Rule> regexs = new HashMap<>();
 	
 	@Override
 	public Grammar grammar(List<Object> rules) {
@@ -48,21 +52,20 @@ public abstract class Builder implements iguana.parsetrees.iggy.Builder {
 	}
 	
 	@Override
-	public List<Rule> rule(List<Object> tag, Object name, List<Object> params, List<Object> body) {
-		// TODO: Add the logic of handling precedence
+	public List<Rule> rule(List<Object> tag, Object name, List<Object> params, List<Object> body) { // TODO: Add the logic of handling precedence
 		List<Rule> rules = new ArrayList<>();
 		body.forEach(elem -> {
-			PrecG g = (PrecG) elem;
-			g.alts.forEach(alt -> {
+			((PrecGroup)elem).alts.forEach(alt -> {
 				final Nonterminal head = params.isEmpty() ? Nonterminal.withName((String)name)
-						                   : Nonterminal.builder((String)name).addParameters(params.stream().map(p -> (String)p).toArray(String[]::new))
-						                       .build();
+                        : Nonterminal.builder((String)name)
+                              .addParameters(params.stream().map(p -> (String)p).toArray(String[]::new))
+                              .build();
 				if (alt instanceof Sequence) {
 					org.iguana.grammar.symbol.Rule.Builder builder = Rule.withHead(head);
 					builder.addSymbols(((Sequence)alt).syms);
 					rules.add(builder.build());
-				} else if (alt instanceof AssocG) {
-					((AssocG)alt).seqs.forEach(seq -> {
+				} else if (alt instanceof AssocGroup) {
+					((AssocGroup)alt).seqs.forEach(seq -> {
 						org.iguana.grammar.symbol.Rule.Builder builder = Rule.withHead(head);
 						builder.addSymbols(seq.syms);
 						rules.add(builder.build());
@@ -75,17 +78,20 @@ public abstract class Builder implements iguana.parsetrees.iggy.Builder {
 	
 	@Override
 	public Object rule(Object name, Object body) {
-		// TODO:
-		return null;
+        Rule.Builder builder = Rule.withHead(Nonterminal.withName((String)name));
+        builder.addSymbol((Symbol)body);
+        Rule rule = builder.build();
+        regexs.put((String)name, rule);
+        return rule;
 	}
 	
 	@Override
-	public Object precG(List<Object> alts) {
-		return new PrecG(alts);
+	public Object precGroup(List<Object> alts) {
+		return new PrecGroup(alts);
 	}
 	
 	@Override
-	public Object assocG(List<Object> elems) {
+	public Object assocGroup(List<Object> elems) {
 		List<Sequence> seqs = new ArrayList<>();
 		List<String> assoc = new ArrayList<>();
 		elems.forEach(elem -> {
@@ -94,7 +100,7 @@ public abstract class Builder implements iguana.parsetrees.iggy.Builder {
 			else if (elem instanceof String)
 				assoc.add((String) elem);
 		});
-		return new AssocG(seqs, assoc.get(0));
+		return new AssocGroup(seqs, assoc.get(0));
 	}
 	
 	@Override
@@ -112,46 +118,81 @@ public abstract class Builder implements iguana.parsetrees.iggy.Builder {
 		return new Sequence(syms, attrs);
 	}
 	
-	/*
-	 * EBNF related
-	 */
-	
 	@Override
-	public Object star(Object arg0) {
-		// TODO:
-		return null;
+	public Object star(Object sym) {
+        return Star.from((Symbol)sym);
 	}
 	
 	@Override
-	public Object plus(Object arg0) {
-		// TODO:
-		return null;
+	public Object plus(Object sym) {
+		return Plus.from((Symbol)sym);
 	}
 	
 	@Override
-	public Object opt(Object arg0) {
-		// TODO:
-		return null;
+	public Object opt(Object sym) {
+		return Opt.from((Symbol)sym);
 	}
 	
 	@Override
-	public Object seqG(List<Object> arg0) {
-		// TODO:
-		return null;
+	public Object seqGroup(List<Object> syms) {
+        return org.iguana.regex.Sequence.from(syms.stream().map(sym -> (Symbol)sym).collect(Collectors.toList()));
 	}
 	
 	@Override
-	public Object altG(List<Object> arg0) {
-		// TODO:
-		return null;
+	public Object altGroup(List<Object> elems) {
+        List<Symbol> syms = new ArrayList<>();
+        for (Object elem : elems) {
+            if (elem instanceof Symbol)
+                syms.add((Symbol)elem);
+            else if (elem instanceof Sequence)
+                syms.add(org.iguana.regex.Sequence.from(((Sequence)elem).syms));
+        }
+		return Alt.from(syms);
 	}
-	
-	/*
-	 * Symbols
-	 */
-	
-	@Override
-	public Nonterminal callS(Object sym, List<Object> args) {
+
+    @Override
+    public Object syms(List<Object> syms) {
+        if (syms.isEmpty())
+            return Epsilon.getInstance();
+
+        if (syms.size() == 1)
+            return (Symbol)syms.get(0);
+
+        return new Sequence(syms.stream().map(sym -> (Symbol)sym).collect(Collectors.toList()), null);
+    }
+
+    @Override
+    public Object regStar(Object s) {
+        return star(s);
+    }
+
+    @Override
+    public Object regPlus(Object s) {
+        return plus(s);
+    }
+
+    @Override
+    public Object regOpt(Object s) {
+        return opt(s);
+    }
+
+    @Override
+    public Object regSeqGroup(List<Object> ss) {
+        return seqGroup(ss);
+    }
+
+    @Override
+    public Object regAltGroup(List<Object> ss) {
+        return altGroup(ss);
+    }
+
+    @Override
+    public Object regs(List<Object> ss) {
+        return syms(ss);
+    }
+
+    @Override
+	public Nonterminal nontCall(Object sym, List<Object> args) {
 		org.iguana.grammar.symbol.Nonterminal.Builder builder = Nonterminal.builder((Nonterminal) sym);
 		if (args.isEmpty()) 
 			return builder.build();
@@ -177,48 +218,34 @@ public abstract class Builder implements iguana.parsetrees.iggy.Builder {
 	
 	@Override
 	public org.iguana.regex.Sequence<org.iguana.grammar.symbol.Character> string(Object obj) {
-		String s = (String) obj;
-		s = s.substring(1, s.length() - 1);
-		return org.iguana.regex.Sequence.from(s.chars().toArray());
-	}
-	
-	@Override
-	public Object character(Object obj) {
-		String s = (String) obj;
-		s = s.substring(1, s.length() - 1);
-		return org.iguana.grammar.symbol.Character.from(s.charAt(0));
-	}
-	
+        String s = (String) obj;
+        s = s.substring(1, s.length() - 1);
+        return org.iguana.regex.Sequence.from(s.chars().toArray());
+    }
 	
 	/*
 	 *  Helper classes
 	 */
 	
-	public static class PrecG {
-		
+	public static class PrecGroup {
 		public final List<Object> alts;
-		
-		public PrecG(List<Object> alts) {
+		public PrecGroup(List<Object> alts) {
 			this.alts = alts;
 		}
 	}
 	
-	public static class AssocG {
-		
+	public static class AssocGroup {
 		public final List<Sequence> seqs;
 		public final String assoc;
-		
-		public AssocG(List<Sequence> seqs, String assoc) {
+		public AssocGroup(List<Sequence> seqs, String assoc) {
 			this.seqs = seqs;
 			this.assoc = assoc;
 		}
 	}
 	
 	public static class Sequence {
-		
 		public final List<Symbol> syms;
 		public final List<String> attrs;
-		
 		public Sequence(List<Symbol> syms, List<String> attrs) {
 			this.syms = syms;
 			this.attrs = attrs;
