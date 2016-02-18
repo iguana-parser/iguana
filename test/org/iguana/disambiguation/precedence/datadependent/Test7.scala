e2e079bcc6929f24bcf48ca4e7fc68bd0e0687a2
Anastassija <A.Izmaylova@cwi.nl>
2016-02-18 23:39:43 +0100

    Converted more precedence related tests.

---
+++ test/org/iguana/disambiguation/precedence/datadependent/Test7.scala
@@ -0,0 +1,57 @@
+package org.iguana.disambiguation.precedence.datadependent
+
+import iguana.utils.input.Input
+import org.iguana.IGGY
+import org.iguana.grammar.iggy.IggyParser
+import org.iguana.grammar.symbol.Nonterminal
+import org.iguana.parser.Iguana
+import org.scalatest.FunSuite
+
+/**
+  * Created by Anastasia Izmaylova
+  */
+class Test7 extends FunSuite {
+
+  val originalGrammar = {
+    @IGGY
+    val s =
+      """
+        | S ::= E
+        | E ::= E 'z'
+        |     > 'x' E
+        |     > E 'w'
+        |     | 'a'
+      """.stripMargin
+    IggyParser.getGrammar(s)
+  }
+
+  val start = Nonterminal.withName("S")
+
+  val desugaredGrammar = {
+    @IGGY
+    val s =
+      """
+        | S ::= E(0)
+        | E(p) ::= [3>=p] l=E(p) [l<=0||l>=3] 'z' {0}
+        |        | 'x' E(2) {2}
+        |        | [1>=p] l=E(p) [l<=0||l>=1] 'w' {0}
+        |        | 'a' {0}
+      """.stripMargin
+    IggyParser.getGrammar(s)
+  }
+
+  test("Parser") {
+    val input = Input.fromString("xawz")
+    val result = Iguana.parse(input, originalGrammar, start)
+    assert(result.isParseSuccess)
+    assertResult(0)(result.asParseSuccess.getStatistics.getCountAmbiguousNodes)
+  }
+
+  test("DDParser") {
+    val input = Input.fromString("xawz")
+    val result = Iguana.parse(input, desugaredGrammar, start)
+    assert(result.isParseSuccess)
+    assertResult(0)(result.asParseSuccess.getStatistics.getCountAmbiguousNodes)
+  }
+
+}
