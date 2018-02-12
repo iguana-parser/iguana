c727695c151ff3b0a80119fb40e1bfffd402587a
Ali Afroozeh <afroozeh@gmail.com>
2018-02-12 08:20:50 +0100

    Change the interface of GraphVizUtil

--- src/main/java/iguana/utils/collections/CollectionsUtil.java
+++ src/main/java/iguana/utils/collections/CollectionsUtil.java
@@ -197,4 +197,11 @@ public class CollectionsUtil {
 	    return max;
     }
 
+    public static <T> List<T> concat(List<T> list1, List<T> list2) {
+		List<T> list = new ArrayList<>(list1.size() + list2.size());
+		list.addAll(list1);
+		list.addAll(list2);
+		return list;
+	}
+
 }
