937e4bc5a94f29bf0f970d81bfa587fa6607c61a
Ali Afroozeh <afroozeh@gmail.com>
2018-02-14 21:58:13 +0100

    Add support for generating parse trees for ambiguity under intermediate node

--- src/org/iguana/sppf/SPPFParseTreeVisitor.java
+++ src/org/iguana/sppf/SPPFParseTreeVisitor.java
@@ -13,11 +13,9 @@ import java.util.stream.Stream;
 
 public class SPPFParseTreeVisitor implements SPPFVisitor<List<Object>> {
 
-    private final Input input;
     private final ParseTreeBuilder parseTreeBuilder;
 
-    public SPPFParseTreeVisitor(Input input, ParseTreeBuilder parseTreeBuilder) {
-        this.input = input;
+    public SPPFParseTreeVisitor(ParseTreeBuilder parseTreeBuilder) {
         this.parseTreeBuilder = parseTreeBuilder;
     }
 
@@ -42,28 +40,30 @@ public class SPPFParseTreeVisitor implements SPPFVisitor<List<Object>> {
 
             Object ambiguityNode = parseTreeBuilder.ambiguityNode(ambiguities);
             result.add(ambiguityNode);
-//            parseTreeBuilder.nonterminalNode()
-//            Set<NonterminalNode> children = new HashSet<>();
-//            for (PackedNode packedNode : node.getChildren()) {
-//                Rule rule = ((EndGrammarSlot)packedNode.getChildAt(0).getGrammarSlot()).getPosition().getRule();
-//                children.add(new NonterminalNode(
-//                        input,
-//                        rule,
-//                        packedNode.accept(this),
-//                        node.getLeftExtent(),
-//                        node.getRightExtent()
-//                ));
-//            }
-//            result.add(new AmbiguityNode(input, children));
         } else {
             switch (node.getGrammarSlot().getNodeType()) {
                 case Basic:
                     PackedNode packedNode = node.getChildAt(0);
                     List<Object> children = new ArrayList<>(packedNode.accept(this));
 
-                    Object nonterminalNode = parseTreeBuilder.nonterminalNode(packedNode.getGrammarSlot().getPosition().getRule(),
-                            children, node.getLeftExtent(), node.getRightExtent());
-                    result.add(nonterminalNode);
+                    // An ambiguity under intermediate node has propagated upwards
+                    if (isListOfList(children)) {
+                        Set<Object> ambiguties = new HashSet<>();
+                        for (Object object : children) {
+                            Object nonterminalNode = parseTreeBuilder.nonterminalNode(
+                                    packedNode.getGrammarSlot().getPosition().getRule(),
+                                    (List<?>) object,
+                                    node.getLeftExtent(),
+                                    node.getRightExtent()
+                            );
+                            ambiguties.add(nonterminalNode);
+                        }
+                        result.add(parseTreeBuilder.ambiguityNode(ambiguties));
+                    } else {
+                        Object nonterminalNode = parseTreeBuilder.nonterminalNode(packedNode.getGrammarSlot().getPosition().getRule(),
+                                children, node.getLeftExtent(), node.getRightExtent());
+                        result.add(nonterminalNode);
+                    }
             }
         }
         return result;
@@ -71,15 +71,16 @@ public class SPPFParseTreeVisitor implements SPPFVisitor<List<Object>> {
 
     @Override
     public List<Object> visit(IntermediateNode node) {
-        Stream<Object> left = node.getChildAt(0).accept(this).stream();
-        Stream<Object> children;
-        if (node.childrenCount() == 2) {
-            Stream<Object> right = node.getChildAt(2).accept(this).stream();
-            children = Stream.concat(left, right);
+        if (node.isAmbiguous()) {
+            List<Object> ambiguities = new ArrayList<>();
+            for (PackedNode packedNode : node.getChildren()) {
+                ambiguities.add(new ArrayList<>(packedNode.accept(this)));
+            }
+            return ambiguities;
         } else {
-            children = left;
+            PackedNode packedNode = node.getChildAt(0);
+            return new ArrayList<>(packedNode.accept(this));
         }
-        return children.filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     @Override
@@ -93,9 +94,61 @@ public class SPPFParseTreeVisitor implements SPPFVisitor<List<Object>> {
         return merge(left, right);
     }
 
+    /*
+     * Case 1: left: List<Object>, right: List<Object>
+     * case 2: List<List<Object>>, right: List<Object>
+     *
+     */
     private static List<Object> merge(List<Object> list1, List<Object> list2) {
         if (list1 == null || list2 == null) throw new IllegalArgumentException("list1 or list2 cannot be empty");
-        return Stream.concat(list1.stream(), list2.stream()).filter(Objects::nonNull).collect(Collectors.toList());
+
+        List<Object> result = new ArrayList<>();
+
+        if (isListOfList(list1) && !isListOfList(list2)) {
+            for (Object object : list1) {
+                List<Object> newList = new ArrayList<>();
+                addAllNonNull(newList, (List<Object>) object);
+                addAllNonNull(newList, list2);
+                result.add(newList);
+            }
+        }
+
+        else if (!isListOfList(list1) && isListOfList(list2)) {
+            for (Object object : list2) {
+                List<Object> newList = new ArrayList<>();
+                addAllNonNull(newList, (List<Object>) object);
+                addAllNonNull(newList, list1);
+                result.add(newList);
+            }
+        }
+
+        else if (isListOfList(list1) && isListOfList(list2)) {
+            for (Object object1: list1)
+                for (Object object2: list2) {
+                    List<Object> newList = new ArrayList<>();
+                    addAllNonNull(newList, (List<Object>) object1);
+                    addAllNonNull(newList, (List<Object>) object2);
+                    result.add(newList);
+                }
+        }
+
+        else {
+            addAllNonNull(result, list1);
+            addAllNonNull(result, list2);
+        }
+
+        return result;
+    }
+
+    private static boolean isListOfList(List<Object> list) {
+        return list.size() > 0 && list.get(0) instanceof List<?>;
+    }
+
+    private static <T> void addAllNonNull(List<T> to, List<T> from) {
+        for (T o : from) {
+            if (o != null)
+                to.add(o);
+        }
     }
 
 }
