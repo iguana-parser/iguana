55a4d3b5be73375645ad837ed86ac1cc8142c457
Ali Afroozeh <afroozeh@gmail.com>
2018-08-09 01:04:00 +0200

    Remove unused static maps

--- src/main/java/iguana/regex/CharacterRanges.java
+++ src/main/java/iguana/regex/CharacterRanges.java
@@ -33,22 +33,6 @@ import static iguana.utils.collections.CollectionsUtil.toList;
 
 public class CharacterRanges {
 
-	public static Set<CharRange> toNonOverlappingSet(Iterable<CharRange> ranges) {
-		return toNonOverlapping(toList(ranges)).values().stream().flatMap(l -> l.stream()).collect(Collectors.toSet());
-	}
-
-    /**
-	 * 
-	 * @return a map from the new, non-overlapping ranges to original ranges
-	 */
-	public static Map<CharRange, List<CharRange>> toNonOverlapping2(Iterable<CharRange> ranges) {
-		Map<CharRange, List<CharRange>> nonOverlapping = toNonOverlapping(toList(ranges));
-		Map<CharRange, List<CharRange>> map = new HashMap<>();
-		nonOverlapping.forEach((k, v) -> v.forEach(r -> map.computeIfAbsent(r, key -> new ArrayList<>()).add(k)));
-		return map;
-	}
-
-	
 	public static Map<CharRange, List<CharRange>> toNonOverlapping(Iterable<CharRange> ranges) {
 		return toNonOverlapping(toList(ranges));
 	}
@@ -136,34 +120,6 @@ public class CharacterRanges {
 
     public static final int MAX_UTF32_VAL = 0x10FFFF;
 
-    public static String getTypeName(int codePoint) {
-        return categoryNames.get(java.lang.Character.getType(codePoint));
-    }
-
-    public static Set<Integer> getValuesInCategory(String categoryName) {
-        return categoriesMap.get(categoryName);
-    }
-
-    public static Alt<CharRange> getCharacterClassInCategory(String categoryName) {
-        List<Integer> vals = new ArrayList<>(categoriesMap.get(categoryName));
-        Collections.sort(vals);
-
-        List<CharRange> ranges = new ArrayList<>();
-
-        int start = vals.get(0);
-        int end = vals.get(0);
-
-        for (int i = 1; i < vals.size(); i++) {
-            if (vals.get(i) - end > 1) {
-                ranges.add(CharRange.in(start, end));
-                start = vals.get(i);
-            }
-            end = vals.get(i);
-        }
-
-        return Alt.from(ranges);
-    }
-
     public static boolean isPrintableAscii(int codePoint) {
         return '\u0020' < codePoint && codePoint < '\u007f';
     }
@@ -179,80 +135,4 @@ public class CharacterRanges {
         return Alt.from(ranges);
     }
 
-    private static Map<Byte, String> categoryNames = new HashMap<>();
-
-    private static Map<String, Set<Integer>> categoriesMap = new HashMap<>();
-
-    static {
-        categoryNames.put(java.lang.Character.UNASSIGNED, "Cn");
-
-        categoryNames.put(java.lang.Character.UPPERCASE_LETTER, "Lu");
-
-        categoryNames.put(java.lang.Character.LOWERCASE_LETTER, "Ll");
-
-        categoryNames.put(java.lang.Character.TITLECASE_LETTER, "Lt");
-
-        categoryNames.put(java.lang.Character.MODIFIER_LETTER, "Lm");
-
-        categoryNames.put(java.lang.Character.OTHER_LETTER, "Lo");
-
-        categoryNames.put(java.lang.Character.NON_SPACING_MARK, "Mn");
-
-        categoryNames.put(java.lang.Character.ENCLOSING_MARK, "Me");
-
-        categoryNames.put(java.lang.Character.COMBINING_SPACING_MARK, "Mc");
-
-        categoryNames.put(java.lang.Character.DECIMAL_DIGIT_NUMBER, "Nd");
-
-        categoryNames.put(java.lang.Character.LETTER_NUMBER, "Nl");
-
-        categoryNames.put(java.lang.Character.OTHER_NUMBER, "No");
-
-        categoryNames.put(java.lang.Character.SPACE_SEPARATOR, "Zs");
-
-        categoryNames.put(java.lang.Character.LINE_SEPARATOR, "Zl");
-
-        categoryNames.put(java.lang.Character.PARAGRAPH_SEPARATOR, "Zp");
-
-        categoryNames.put(java.lang.Character.CONTROL, "Cc");
-
-        categoryNames.put(java.lang.Character.FORMAT, "Cf");
-
-        categoryNames.put(java.lang.Character.PRIVATE_USE, "Co");
-
-        categoryNames.put(java.lang.Character.SURROGATE, "Cs");
-
-        categoryNames.put(java.lang.Character.DASH_PUNCTUATION, "Pd");
-
-        categoryNames.put(java.lang.Character.START_PUNCTUATION, "Ps");
-
-        categoryNames.put(java.lang.Character.END_PUNCTUATION, "Pe");
-
-        categoryNames.put(java.lang.Character.CONNECTOR_PUNCTUATION, "Pc");
-
-        categoryNames.put(java.lang.Character.OTHER_PUNCTUATION, "Po");
-
-        categoryNames.put(java.lang.Character.MATH_SYMBOL, "Sm");
-
-        categoryNames.put(java.lang.Character.CURRENCY_SYMBOL, "Sc");
-
-        categoryNames.put(java.lang.Character.MODIFIER_SYMBOL, "Sk");
-
-        categoryNames.put(java.lang.Character.OTHER_SYMBOL, "So");
-
-        categoryNames.put(java.lang.Character.INITIAL_QUOTE_PUNCTUATION, "Pi");
-
-        categoryNames.put(java.lang.Character.FINAL_QUOTE_PUNCTUATION, "Pf");
-
-        for (int i = 0; i <= MAX_UTF32_VAL; i++) {
-            String categoryName = categoryNames.get((byte) java.lang.Character.getType(i));
-            Set<Integer> set = categoriesMap.get(categoryName);
-            if (set == null) {
-                set = new HashSet<>();
-                categoriesMap.put(categoryName, set);
-            }
-            set.add(i);
-        }
-    }
-	
 }
