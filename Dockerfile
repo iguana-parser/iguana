df8709995cdb9fcc37d118bbd3fd6080f1e9724d
Pogozhelskaya Vlada <pogozhelskaya@gmail.com>
2021-08-26 17:24:34 +0300

    configurating docker

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY . /app
 RUN mvn clean install -DskipTests
 #RUN mvn exec:java -Dexec.mainClass="benchmark.Neo4jBenchmark" -Dexec.args="bt 1 2 5 /Users/vladapogozhelskaya/Downloads/neo4j-enterprise-4.0.12 test/resources/grammars/graph/Test1/grammar.json geospecies" -Dexec.cleanupDaemonThreads=false
 RUN chmod +x entrypoint.sh
-ENTRYPOINT ["/bin/bash", "entrypoint.sh"]
+# ENTRYPOINT ["/bin/bash", "entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-#mvn exec:java -Dexec.mainClass="benchmark.Neo4jBenchmark" -Dexec.args="bt 450609 2 5 /Users/vladapogozhelskaya/Downloads/neo4j-enterprise-4.0.12 test/resources/grammars/graph/Test1/grammar.json geospecies" -Dexec.cleanupDaemonThreads=false
+mvn exec:java -Dexec.mainClass="benchmark.Neo4jBenchmark" -Dexec.args="bt 450609 2 5 /Users/vladapogozhelskaya/Downloads/neo4j-enterprise-4.0.12 test/resources/grammars/graph/Test1/grammar.json geospecies" -Dexec.cleanupDaemonThreads=false
