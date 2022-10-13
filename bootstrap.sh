71cb79816a24b9c3d4cf10b0073f5eb10e3685e2
Ali Afroozeh <afroozeh@gmail.com>
2022-10-13 21:18:14 +0200

    add explicit output location for grammar.json file

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ java -jar target/iguana-0.1-SNAPSHOT.jar\
   --generate-types\
   --name iggy\
   --grammar src/resources/Iguana.iggy\
+  --grammar-output src/resources\
   --output src\
   --package org.iguana.iggy.gen
